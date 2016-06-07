08237420771bf3a4818d915b8ba2e6e649337712
carolinanery <carolina.nery94@gmail.com>
2016-06-07 10:42:54 -0300

    Teste unitário PRJ-8

--- otus-rest/src/test/java/br/org/otus/rest/ResponseTest.java
+++ otus-rest/src/test/java/br/org/otus/rest/ResponseTest.java
@@ -1,35 +1,32 @@
-package br.org.studio.rest;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-
-import br.org.otus.rest.Response;
-import br.org.otus.rest.dtos.OtusInitializationConfigDto;
+package br.org.otus.rest;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import com.google.gson.Gson;
 
+import br.org.otus.rest.dtos.OtusInitializationConfigDto;
+
 public class ResponseTest {
 
-	//TODO
-	
+	// TODO
+
 	private OtusInitializationConfigDto otusInitConfig;
-	
+
 	@Before
-	public void setup(){
+	public void setup() {
 		otusInitConfig = new OtusInitializationConfigDto();
 		otusInitConfig.setDomainRestUrl("domainRestUrl");
 		otusInitConfig.setProjectName("projectName");
 	}
-	
+
 	@Test
-	public void toString_should_return_a_json_with_value_of_data_equal_to_true(){
+	public void toString_should_return_a_json_with_value_of_data_equal_to_true() {
 		Response response = new Response();
 		response.setData(true);
-		
+
 		Gson gson = new Gson();
 		Response generatedJson = gson.fromJson(new Gson().toJson(response), Response.class);
-		//assertThat(generatedJson.getData(), equalTo(Boolean.TRUE));
+
 	}
 }
