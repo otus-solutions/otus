36c44adab9ca042aa6603abe61d739e948bde039
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-07-28 16:48:33 -0300

    USR-3 - modificações realizadas no cadastro inicial, utilizando emailSender

--- otus-business/src/main/java/br/org/otus/configuration/dto/OtusInitializationConfigDto.java
+++ otus-business/src/main/java/br/org/otus/configuration/dto/OtusInitializationConfigDto.java
@@ -1,25 +1,35 @@
 package br.org.otus.configuration.dto;
 
 import br.org.otus.domain.DomainDto;
+import br.org.otus.email.dto.EmailSenderDto;
 import br.org.otus.project.dto.ProjectDto;
 import br.org.otus.user.dto.UserDto;
 
 public class OtusInitializationConfigDto {
 
-    private UserDto user;
-    private ProjectDto project;
-    private DomainDto domain;
+	private UserDto user;
+	private EmailSenderDto emailSender;
+	private ProjectDto project;
+	private DomainDto domain;
 
-    public UserDto getUser() {
-        return user;
-    }
+	public UserDto getUser() {
+		return user;
+	}
 
-    public ProjectDto getProject() {
-        return project;
-    }
+	public ProjectDto getProject() {
+		return project;
+	}
 
-    public DomainDto getDomainDto() {
-        return domain;
-    }
+	public DomainDto getDomainDto() {
+		return domain;
+	}
+
+	public EmailSenderDto getEmailSender() {
+		return emailSender;
+	}
+
+	public void setEmailSender(EmailSenderDto emailSender) {
+		this.emailSender = emailSender;
+	}
 
 }
