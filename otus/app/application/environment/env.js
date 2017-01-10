4ed2a6e7956b9dfc55b5865abddccdd1efdc847e
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2017-01-10 20:54:57 -0200

    Update env.js
    Adição de valor default para env.js

--- otus/app/application/environment/env.js
+++ otus/app/application/environment/env.js
@@ -2,5 +2,5 @@
   'use strict';
 
   window.__env = window.__env || {};
-  window.__env.apiUrl = 'https://api-otus.hmg.ccem.ufrgs.br';
+  window.__env.apiUrl = 'http://api-domain.localhost:8080';
 }(this));
