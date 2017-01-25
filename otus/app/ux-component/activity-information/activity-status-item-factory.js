ee8bee11e0ff2271ca36c6e3f7a8032cd65bad7b
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2017-01-25 12:01:54 -0200

    Adição de Icones para Histórico de Atividade

--- otus/app/ux-component/activity-information/activity-status-item-factory.js
+++ otus/app/ux-component/activity-information/activity-status-item-factory.js
@@ -21,30 +21,46 @@
   function ActivityStatusItem(data) {
     var self = this;
 
-    self.name = _getStatusName(data);
     self.date = _getFormattedDate(data);
     self.user = data.user;
     self.user.name = data.user.name + ' ' + data.user.surname;
+    self.status = _buildStatus(data);
 
-    function _getStatusName(status) {
-      if ('CREATED' === status.name) {
-        return 'Criado';
-      }
-      if ('OPENED' === status.name) {
-        return 'Aberto';
-      }
-      if ('FINALIZED' === status.name) {
-        return 'Finalizado';
-      }
-      if ('SAVED' === status.name) {
-        return 'Salvo';
-      }
-      if ('INITIALIZED_OFFLINE' === status.name) {
-        return 'Realizado em Papel';
-      }
-      if ('INITIALIZED_ONLINE' === status.name) {
-        return 'Iniciado Online';
-      }
+    function _buildStatus(status) {
+      var map = {
+        CREATED: {
+          label: 'Criado',
+          color: '#595959',
+          icon: 'fiber_new'
+        },
+        OPENED: {
+          label: 'Aberto',
+          color: '#0091ea',
+          icon: 'visibility'
+        },
+        FINALIZED: {
+          label: 'Finalizado',
+          color: '#009688',
+          icon: 'check_circle'
+        },
+        SAVED: {
+          label: 'Salvo',
+          color: '#ae8323',
+          icon: 'save'
+        },
+        INITIALIZED_OFFLINE: {
+          label: 'Realizado em Papel',
+          color: '#886430',
+          icon: 'edit'
+        },
+        INITIALIZED_ONLINE: {
+          label: 'Iniciado Online',
+          color: '#513c1c',
+          icon: 'edit'
+        }
+      };
+
+      return map[status.name];
     }
 
     function _getFormattedDate(status) {
