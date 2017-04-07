098b3d940c94f9e4338606860e595750310b8149
emanoelvianna <emanoel.vianna@outlook.com>
2017-04-07 16:26:05 -0300

    otus-195 - adicionado mecanismo para tratar o nome do participante

--- otus/app/module/laboratory/business/participant-laboratory-factory.js
+++ otus/app/module/laboratory/business/participant-laboratory-factory.js
@@ -30,12 +30,12 @@
       var json = {};
 
       json.recruitment_number = participant.recruitmentNumber;
-      json.participant_name = participant.name;
+      json.participant_name = _buildNameParticipantLabel(participant.name);
       json.gender = participant.sex;
       json.birthday = _convertFormatDate(new Date(participant.birthdate.value));
       json.cq_group = (laboratory.collectGroupName !== undefined && laboratory.collectGroupName !== DEFAULT) ? laboratory.collectGroupName : NONE;
       json.tubes = laboratory.tubes;
-      buildTubeLabel(json.tubes);
+      _buildTubeLabel(json.tubes);
       return JSON.stringify(json);
     }
 
@@ -44,22 +44,27 @@
       return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
     }
 
-    function buildTubeLabel(tubes) {
+    function _buildTubeLabel(tubes) {
       var labels = {};
-      labels.GEL = "Gel";
-      labels.FLUORIDE = "Fluoreto";
-      labels.EDTA = "EDTA";
-      labels.URINE = "Urina";
-      labels.CITRATE = "Citrato";
+      labels.GEL = 'Gel';
+      labels.FLUORIDE = 'Fluoreto';
+      labels.EDTA = 'EDTA';
+      labels.URINE = 'Urina';
+      labels.CITRATE = 'Citrato';
 
-      labels.FASTING = "Jejum";
-      labels.ONE_HOUR = "1 Hora";
-      labels.POST_OVERLOAD = "Pós";
-      labels.NONE = "";
+      labels.FASTING = 'Jejum';
+      labels.ONE_HOUR = '1 Hora';
+      labels.POST_OVERLOAD = 'Pós';
+      labels.NONE = '';
 
       tubes.forEach(function(tube) {
-        tube.label = labels[tube.type] + " " + labels[tube.moment];
+        tube.label = labels[tube.type] + ' ' + labels[tube.moment];
       });
+    }
+
+    function _buildNameParticipantLabel(name) {
+      var result = name.split(' ');
+      return result[0] + ' ' + result[result.length - 1];
     }
 
   }
