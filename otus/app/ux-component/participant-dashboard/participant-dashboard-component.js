626826e8f010f19fbc3c266255cd5377bdbf0dfc
Adriano Boese <desenvacc@gmail.com>
2017-07-17 15:35:21 -0300

    OTUS-265 Criaando ambiente home

---
+++ otus/app/ux-component/participant-dashboard/participant-dashboard-component.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('otusjs.otus.uxComponent')
+    .component('otusParticipantDashboard', {
+      controller: Controller,
+      templateUrl: 'app/ux-component/participant-dashboard/participant-dashboard-template.html'
+    });
+
+  Controller.$inject = [
+    'otusjs.application.state.ApplicationStateService'
+  ];
+
+  function Controller(ApplicationStateService) {
+    var self = this;
+
+    /* Public methods */
+    self.selectParticipant = selectParticipant;
+    /* Lifecycle hooks */
+    self.$onInit = onInit;
+
+    function selectParticipant(selectedParticipant) {
+      self.selectedParticipant = selectedParticipant;
+    }
+
+    /* Lifecycle methods */
+    function onInit() {
+      self.selectedParticipant = null;
+    }
+  }
+}());
