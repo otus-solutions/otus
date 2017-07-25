9605433a6a994cf207447bdbe0ec50d6c2962d2e
Adriano Boese <desenvacc@gmail.com>
2017-07-25 16:34:08 -0300

    OTUS-265 erro ao fazer login

--- otus/app/deploy/state/dashboard/dashboard-state-provider.js
+++ otus/app/deploy/state/dashboard/dashboard-state-provider.js
@@ -22,7 +22,34 @@
       parent: STATE.SESSION,
       name: STATE.DASHBOARD,
       url: '/' + STATE.DASHBOARD,
-      template: '<otus-dashboard layout="column" flex></otus-dashboard>'
+      template: '<otus-dashboard layout="column" flex></otus-dashboard>',
+      data: {
+        redirect: _redirect
+      }
     };
+
+    function _redirect($q, DashboardContextService, Application) {
+      var deferred = $q.defer();
+
+      Application
+        .isDeployed()
+        .then(function() {
+          try {
+            DashboardContextService.isValid();
+            deferred.resolve();
+          } catch (e) {
+            deferred.resolve(STATE.LOGIN);
+          }
+        });
+
+      return deferred.promise;
+    }
+
+    _redirect.$inject = [
+      '$q',
+      'otusjs.otus.dashboard.core.ContextService',
+      'otusjs.application.core.ModuleService'
+    ];
+
   }
 }());
