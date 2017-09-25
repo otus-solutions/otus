c5bb21472446e8a800beb4c44294934872eb340d
Tiago Matana <tiago.matana@gmail.com>
2017-09-25 13:04:43 -0300

    OTUS-293 add Toast message

--- otus/app/ux-component/dashboard-toolbar/dashboard-toolbar-component.js
+++ otus/app/ux-component/dashboard-toolbar/dashboard-toolbar-component.js
@@ -14,17 +14,18 @@
 
   Controller.$inject = [
     'otusjs.otus.dashboard.core.ContextService',
-    'otusjs.otus.dashboard.core.EventService'
+    'otusjs.otus.dashboard.core.EventService',
+    '$mdToast'
   ];
 
-  function Controller(ContextService, EventService) {
+  function Controller(ContextService, EventService, $mdToast) {
     var self = this;
 
     /* Public methods */
     self.selectParticipant = selectParticipant;
     self.copyToClipboard = copyToClipboard;
     self.getKey = getKey;
-    self.key = false;
+    self.key = true;
     self.$onInit = onInit;
 
     function selectParticipant(selectedParticipant) {
@@ -56,11 +57,21 @@
       $temp.val("1bbd70dc1b6fc84e5617ca8703c72c744b3b4fc1").select();
       document.execCommand("copy");
       $temp.remove();
+      showToast("Token copiado!", 3000);
 
     }
 
     function getKey() {
       self.key = true;
+    }
+
+    function showToast(msg, delay) {
+      $mdToast.show(
+        $mdToast.simple()
+        .position("bottom right")
+        .textContent(msg)
+        .hideDelay(delay)
+      );
     }
   }
 }());
