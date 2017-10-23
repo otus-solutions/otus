56020df2befd2473368ebbc92092fd86e4369bda
Allister T. Ramos <allistertr@hotmail.com>
2017-10-23 19:14:46 -0200

    OTUS-302 - Construção da Tabela Dinâmica

--- otus/app/ux-component/dynamic-data-table/dynamic-data-table-component.js
+++ otus/app/ux-component/dynamic-data-table/dynamic-data-table-component.js
@@ -6,13 +6,16 @@
     .component('dynamicDataTable', {
       templateUrl: 'app/ux-component/dynamic-data-table/dynamic-data-table-template.html',
       bindings: {
-        dataSet: '=',
-        dataSetProperties: '=',
-        orderIndex: '=',
+        elementsArray: '<', 
+        elementsProperties: '<',
+        orderIndices: '<',
+        flexArray: '<',
         checkBox: '=',
-        changeOrdenation: '=',
+        enableReorder: '<',
         callBack: '=',
-        headers: '=',
+        //groupBy: '=',
+        headers: '='
+
       },
       controller: Controller
     });
@@ -27,54 +30,118 @@
 
     
 
-    self.headerTable;
-    self.bodyTable;
-    self.error;
+    self.table;
+
     self.orderQuery;
+    self.error;
 
     self.$onInit = onInit;
 
     self.myFunction = myFunction;
+    self.creacteTable = creacteTable;
 
     function onInit() {
       self.myFunction();
-
-      self.headerTable = self.headers;
-      self.orderQuery = [];
-
+      _setOrderQuery();
+      creacteTable();
+      
       self.error = {
         isError: false,
         msg: "Devem ser informadas a mesma quantidade de valores e de cabeçalhos."
       };
-
-      self.header = ['Aqui','Ali','Lá'];
+      console.log(self);
     }
 
-    _getValueToObject(object,property){
-      return object
+    function creacteTable(){
+      self.table = {
+        headers:self.headers,
+        rows: []
+      };
+
+      self.elementsArray.forEach(function(element, index) {
+        self.table.rows.push(
+          _createRow(element, index)
+        );
+      }, this);
     }
 
-    function _createRow(element){
+    function _setOrderQuery(columnName){
+      if(columnName){
+        self.orderQuery = columnName + '.value';
+      } else {
+        self.orderQuery = [];
+        if(self.orderIndices){
+          self.orderIndices.forEach(function(orderIndex){
+            self.orderQuery.push('column' + orderIndex + '.value');
+          }); 
+        }
+      }
+    }
+
+
+
+    function _getObjectProperty(object,property){
+      return object[property];
+    }
+
+    function _getValueFromElement(element,compositeProperty){
+      var propertyArray = compositeProperty.split('.');
+      var value = undefined;
+
+      propertyArray.forEach(function(property) {
+        if(value === undefined){
+          value = _getObjectProperty(element, property);
+        } else {
+          value = _getObjectProperty(value, property);
+        }
+      }, this);
+
+      return value;
+    }
+
+    function clearError(){
+      self.error.isError = false;
+      self.error.msg = '';
+    }
+
+    function setError(msg){
+      self.error.isError = true;
+      self.error.msg = msg;
+    }
+
+
+    function _createRow(element, index){
       var row = {
         type:"dynamicDataTableRow",
         ref: element,
-        columns: []
+        columns: [],
+        index: index,
       };
 
-
-
+      self.elementsProperties.forEach(function(elementProperty, index){
+        var column = _createColumn(
+          row,
+          _getValueFromElement(element,elementProperty),
+          index
+        );
+        
+        row.columns.push(column);
+        row[column.name] = column;
+      }, this);
+      
       return row;
     }
 
     function _createColumn(row, value, index){
       var column = {
         type:"dynamicDataTableColumn",
-        row: row,
+        //row: row,
         value: value,
-        index: index
+        index: index,
+        name: "column" + index
       };
 
-      return structure
+      return column;
     }
 
     function myFunction() {
