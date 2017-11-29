2c836a525cb54079bb9af05a167e06eec4f247c9
Tiago Matana <tiago.matana@gmail.com>
2017-11-29 11:46:23 -0200

    add src

--- otus/gulpfile.js
+++ otus/gulpfile.js
@@ -18,6 +18,7 @@
     var replace = require('gulp-replace');
     var runSequence = require('run-sequence');
     var cleanCSS = require('gulp-clean-css');
+    var addsrc = require('gulp-add-src');
   
     gulp.task('browser-sync', function() {
       browserSync.init({
@@ -54,17 +55,17 @@
   
     gulp.task('compress-compress', function() {
       return gulp.src('app/*.html')
-        .pipe(useref())
-        .pipe(gulpif('*.js', uglify()))
-        .pipe(gulpif('*.css', minifyCss()))
-        .pipe(gulpif('*.css', replace('url(../../static-resource/', 'url(/otus/app/static-resource/')))
-        .pipe(gulpif('index.html', replace('href="css', 'href="dist/otus/css')))
-        .pipe(gulpif('index.html', replace('src="scripts', 'src="dist/otus/scripts')))
+        .pipe(addsrc('app/**/**/*.css'))
         .pipe(useref({
           transformPath: function(filePath) {
             return filePath.replace('/app', '');
           }
         }))
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulpif('*.css', replace('url(../../static-resource/', 'url(/otus/app/static-resource/')))
+        .pipe(gulpif('index.html', replace('href="css', 'href="dist/otus/css')))
+        .pipe(gulpif('index.html', replace('src="scripts', 'src="dist/otus/scripts')))
       .pipe(gulp.dest('dist/otus'));
     });
    
