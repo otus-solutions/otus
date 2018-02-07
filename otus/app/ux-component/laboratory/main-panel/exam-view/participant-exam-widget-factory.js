fba628f9919b9ed1538e4a74598c6255dcaf6c01
brenoscheffer <brenoscheffer@gmail.com>
2018-02-07 16:52:50 -0200

    OTUS-315 Proposta de resolução de promessas do report adcionadas à widget-factory

--- otus/app/ux-component/laboratory/main-panel/exam-view/participant-exam-widget-factory.js
+++ otus/app/ux-component/laboratory/main-panel/exam-view/participant-exam-widget-factory.js
@@ -6,7 +6,26 @@
     .factory('otusjs.otus.uxComponent.ParticipantExamWidgetFactory', factory);
 
 
-  function factory() {
+  factory.$inject = [
+    '$q'
+  ];
 
+  function factory() {
+    var self = this;
+
+    self.name = 'Hemograma';
+    self.isAvailable = null;
+    self.hasBeenDelivered = false;
+    self.requestList = [];
+    self.statusColor = 'red';
+
+
+    $q.all(self.requestList)
+      .then(function (data) {
+        self.isAvailable = true
+      })
+      .catch(function (data) {
+        self.isAvailable = false;
+      });
   }
 }());
