a7dc321fbdc96a745ffdf46916de72130e65f3a6
brenoscheffer <brenoscheffer@gmail.com>
2018-03-20 16:58:12 -0300

    OTUS-341 Teste unitário da widget factory

---
+++ otus/tests/unit/app/module/report/participant-report-widget-factory-spec.js
@@ -0,0 +1,21 @@
+describe('ParticipantReportWidgetFactory', function() {
+
+  var UNIT_NAME = 'otusjs.laboratory.business.participant.aliquot.ParticipantAliquotService';
+  var Mock = {};
+  var Injections = {};
+  var factory = {};
+
+
+  beforeEach(angular.mock.module('otusjs.laboratory.business.participant.aliquot'));
+  beforeEach(function () {
+
+    inject(function (_$injector_) {
+
+      // Injections
+      // service = _$injector_.get(UNIT_NAME, Injections);
+
+    });
+  });
+
+  it('should', function(){});
+});
