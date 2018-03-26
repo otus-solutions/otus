17cdd405f0051b4747c90b275f6d82bdd4d1b7eb
Allister Terhorst Ramos <allistertr@hotmail.com>
2018-03-26 19:34:29 -0300

    OTUS-342 - Componentizando otus-panel

---
+++ otus/app/ux-component/otus-panel/otus-panel-component.js
@@ -0,0 +1,30 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('otusjs.otus.uxComponent')
+    .component('otusPanel', {
+      templateUrl: 'app/ux-component/otus-panel/otus-panel-template.html',
+      bindings: {
+        title: '<',
+        colorLeft: '<',
+        colorRight: '<',
+        align: '<'
+      },
+      transclude: true,
+      controller: Controller
+    });
+
+  function Controller() {
+    var self = this;
+
+    self.$onInit = onInit;
+
+    function onInit(){
+      self.title = self.title || '';
+      self.align = self.align || 'center start';
+      self.colorLeft = self.colorLeft || '#299288';
+      self.colorRight = self.colorRight || '#24baaa';
+    }
+  }
+}());
