84e705f03280cc1c04d008a60430b22d38bb5b79
Tiago Matana <tiago.matana@gmail.com>
2018-07-18 14:17:36 -0300

    Refactor tasks gulpfile

---
+++ otus/gulp-tasks/hash.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var uncache = require('gulp-uncache');
+
+gulp.task('compress-hash', function() {
+  return gulp.src('dist/otus/index.html')
+    .pipe(uncache({
+      append: 'hash',
+      rename: true
+    }))
+    .pipe(gulp.dest('dist/otus'));
+});
