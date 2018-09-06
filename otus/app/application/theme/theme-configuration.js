c315fde57898b308e9eb0a246859b9e3310b8fb8
Adriano <desenvacc@gmail.com>
2018-09-06 18:49:40 -0300

    adicionada a cor branca a paleta de cores accent

--- otus/app/application/theme/theme-configuration.js
+++ otus/app/application/theme/theme-configuration.js
@@ -11,6 +11,12 @@
   ];
 
   function Configuration($mdThemingProvider, $mdIconProvider) {
+    var newLightBlueMap = $mdThemingProvider.extendPalette('light-blue', {
+      '500': '#ffffff',
+      'contrastDarkColors': ['500','A100','A400']
+    });
+    $mdThemingProvider.definePalette('new-light-blue', newLightBlueMap);
+
     $mdThemingProvider.theme('default')
       .primaryPalette('teal', {
         'default': '700',
@@ -18,8 +24,9 @@
         'hue-2': '600',
         'hue-3': '800'
       })
-      .accentPalette('light-blue', {
+      .accentPalette('new-light-blue', {
         'default': 'A700',
+        'hue-1': '500',
         'hue-2': 'A100',
         'hue-3': 'A400'
       })
