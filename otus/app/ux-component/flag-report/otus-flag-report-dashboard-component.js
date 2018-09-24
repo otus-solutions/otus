7a869a3b1034a37116911edf99d9645fe229b9e8
mfrey <mfrey@inf.ufrgs.br>
2018-09-24 12:50:44 -0300

    report de flag modo zoom com dados randomicos para teste funcionando

--- otus/app/ux-component/flag-report/otus-flag-report-dashboard-component.js
+++ otus/app/ux-component/flag-report/otus-flag-report-dashboard-component.js
@@ -21,10 +21,7 @@
     var zoomRect = {};
     var tooltip;
 
-    var margin = { top: 30, right: 10, bottom: 10, left: 10 };
-    var canvas;
-    var x;
-    var y;
+
 
     /* Lifecycle hooks */
     self.$onInit = onInit;
@@ -41,10 +38,15 @@
       generateRandomDataForTesting();
       sortParticipantsByCompletion();
       createOverallFlagReport();
-
+      createZoomedFlagReport();
     }
 
     function createOverallFlagReport() {
+
+      var margin = { top: 30, right: 10, bottom: 10, left: 10 };
+      var canvas;
+      var x;
+      var y;
 
       var height = 900,
         width = height / 4.5;
@@ -136,7 +138,7 @@
         selectedData.splice(0, initialYIndex);
 
         // gera nova visualizacao de sinaleira apenas com os participantes selecionados
-        zoomMatrix.update(selectedData);
+        createZoomedFlagReport(selectedData);
 
       }, false);
 
@@ -166,6 +168,87 @@
 
       // retira a visibilidade da tooltip quando o mouse nao esta posicionado no canvas
       canvasWrapper.addEventListener("mouseout", function (evt) {
+        return tooltip.style("visibility", "hidden");
+      });
+    }
+
+    function createZoomedFlagReport(selectedData) {
+
+      if (!selectedData) {
+        var svg = d3.select("#svg_zoom_id");
+        svg.selectAll("*").remove();
+        return;
+      }
+
+      var margin = { top: 30, right: 10, bottom: 10, left: 10 };
+      var canvas;
+      var x;
+      var y;
+
+      var height = 900,
+        width = height;
+
+      var canvas_matrix_viz = d3.select("#canvas_zoom_id")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+
+      var svg = d3.select("#svg_zoom_id")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+
+      var context = canvas_matrix_viz.node().getContext('2d');
+
+      x = d3.scaleBand()
+        .domain(selectedData.map(function (key, index) {
+          return key.questionnaire;
+        }))
+        .range([0, width]);
+
+      y = d3.scaleBand()
+        .domain(selectedData.map(function (key, index) {
+          return key.participant;
+        }))
+        .range([0, height]);
+
+
+      var colorMap = d3.scaleLinear()
+        .domain([-1, 0, 1, 2])
+        .range(["#ef5545", "white", "#fcff82", "#91ef45"]);
+
+      svg.selectAll("*").remove();
+      zoomRect = svg
+        .append("rect")
+        .style("fill", "black")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", x.range()[1])
+        .attr("height", y.bandwidth())
+        .style("opacity", 0.2);
+
+      selectedData.forEach(function (d, i) {
+        context.beginPath();
+        context.rect(x(d.questionnaire), y(d.participant), x.bandwidth(), y.bandwidth());
+        context.fillStyle = colorMap(d.value);
+        context.fill();
+        context.closePath();
+      });
+
+      canvas = canvas_matrix_viz._groups[0][0];
+
+      document.getElementById('zoom_id').addEventListener('mousemove', function (evt) {
+        var participant = getParticipantFromYCoordinate(canvas, evt, y);
+        var questionnaire = getQuestionnaireFromXCoordinate(canvas, evt, x);
+
+        if (questionnaire && participant) {
+          zoomRect.attr("y", y(participant));
+
+          tooltip.style("visibility", "visible");
+          tooltip.text("Participante: " + participant + "; Questionario: " + questionnaire);
+          tooltip.style("top", (evt.pageY - 10) + "px").style("left", (evt.pageX + 10) + "px");
+        }
+      }, false);
+
+      document.getElementById('zoom_id').addEventListener("mouseout", function (evt) {
         return tooltip.style("visibility", "hidden");
       });
     }
