0c21c30c6410ec4c6b7235d7a1c34060b9f5fe54
Adriano <desenvacc@gmail.com>
2018-12-19 18:27:21 -0200

    OTUS-460 teste parcial do serviço

--- otus/tests/unit/app/module/monitoring/business/laboratory-monitoring-service-spec.js
+++ otus/tests/unit/app/module/monitoring/business/laboratory-monitoring-service-spec.js
@@ -5,17 +5,19 @@ describe('LaboratoryMonitoringService Test', function() {
 
     beforeEach(function() {
         mockInjections();
+
         angular.mock.module('otusjs.monitoring.business', function($provide) {
             $provide.value('otusjs.deploy.LoadingScreenService', {});
-            $provide.value('otusjs.monitoring.business.MonitoringCollectionService', Mock.MonitoringCollectionService);
+            $provide.value('otusjs.model.chart.VerticalBarFactory', Mock.VerticalBarFactory);
+            $provide.value('otusjs.monitoring.repository.MonitoringCollectionService', Mock.MonitoringCollectionService);
         });
 
         inject(function(_$injector_) {
             Injections = {
                 "$q": _$injector_.get('$q'),
                 "LoadingScreenService": _$injector_.get('otusjs.deploy.LoadingScreenService'),
-                "LoadingScreenService": _$injector_.get('otusjs.deploy.LoadingScreenService'),
-                MonitoringCollectionService: Mock.MonitoringCollectionService
+                "VerticalBarFactory": _$injector_.get('otusjs.model.chart.VerticalBarFactory'),
+                "MonitoringCollectionService": _$injector_.get('otusjs.monitoring.repository.MonitoringCollectionService')
             };
 
             service = _$injector_.get('otusjs.monitoring.business.LaboratoryMonitoringService', Injections);
@@ -26,19 +28,61 @@ describe('LaboratoryMonitoringService Test', function() {
         spyOn(Mock.MonitoringCollectionService, "listAcronyms").and.callThrough();
         spyOn(Mock.MonitoringCollectionService, "listCenters").and.callThrough();
         spyOn(Mock.MonitoringCollectionService, "getActivitiesProgressReport").and.callThrough();
+        spyOn(Mock.MonitoringCollectionService, "getDataOfPendingResultsByAliquots").and.callThrough();
     });
 
-    it('should called method create', function() {
+    describe('getDataOfPendingResultsByAliquots Method', function() {
+
+      beforeEach(function() {
+        spyOn(Mock.VerticalBarFactory, "fromJsonObject").and.callThrough();
+      });
+
+      it('should call MonitoringCollectionService.getDataOfPendingResultsByAliquots', function() {
+        service.getDataOfPendingResultsByAliquots("RS");
+        expect(Injections.MonitoringCollectionService.getDataOfPendingResultsByAliquots).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call VerticalBarFactory.fromJsonObject', function() {
+        service.getDataOfPendingResultsByAliquots("RS").then(() => {fail()}).catch(() => {fail()});
+        Injections.MonitoringCollectionService.getDataOfPendingResultsByAliquots("RS").then(() => {
+          expect(Injections.VerticalBarFactory.fromJsonObject).toHaveBeenCalledTimes(1);
+        }).catch(() => {
+          fail()
+        });
+      });
+
+      it('should not call VerticalBarFactory.fromJsonObject', function() {
+        service.getDataOfPendingResultsByAliquots("RS").then(() => {fail()}).catch(() => {fail()});
+        Injections.MonitoringCollectionService.getDataOfPendingResultsByAliquots().then(() => {
+          fail()
+        }).catch(() => {});
+      });
+
+      it('should create promise', function() {
+        service.getDataOfPendingResultsByAliquots("RS");
         expect(Injections.$q.defer).toHaveBeenCalledTimes(1);
+      });
     });
+
 
     function mockInjections() {
         Mock.MonitoringCollectionService = {
+            getDataOfPendingResultsByAliquots: (center) => {
+                if(center === "RS") {
+                  return Promise.resolve();
+                } else{
+                  return Promise.reject();
+                }
+              },
             find: () => {},
             listAcronyms: () => {},
             listCenters: () => {},
             getActivitiesProgressReport: () => {}
-        }
+        };
+
+        Mock.VerticalBarFactory = {
+            fromJsonObject: () => {}
+        };
     }
 
 });
