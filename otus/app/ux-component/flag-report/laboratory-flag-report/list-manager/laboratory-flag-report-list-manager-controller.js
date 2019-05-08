2558159a8ca12f66a5bb9511d57dbc87ca119056
emanoelvianna <emanoel.vianna@outlook.com>
2019-05-08 18:40:22 -0300

    OTUS-529 - relatório de flags utilizando novo formato de gráfico

--- otus/app/ux-component/flag-report/laboratory-flag-report/list-manager/laboratory-flag-report-list-manager-controller.js
+++ otus/app/ux-component/flag-report/laboratory-flag-report/list-manager/laboratory-flag-report-list-manager-controller.js
@@ -229,338 +229,108 @@
       }
     }
 
-    function _heatmap_display(exams, heatmapId) {
-      var svg = d3.select(heatmapId).selectAll("*").remove();
+    function _heatmap_display(exams) {
+      var svg = d3.select("#exam-heatmap").selectAll("*").remove();
+      var fullData = create(exams);
+      var data = fullData.values;
 
-      var tooltip = d3.select(heatmapId)
-        .append("div")
-        .style("position", "absolute")
-        .style("visibility", "hidden");
+      // var data = [{ "group": "A", "variable": "v1", "value": "30" }, { "group": "A", "variable": "v2", "value": "95" }, { "group": "A", "variable": "v3", "value": "22" }, { "group": "A", "variable": "v4", "value": "14" }, { "group": "A", "variable": "v5", "value": "59" }, { "group": "A", "variable": "v6", "value": "52" }, { "group": "A", "variable": "v7", "value": "88" }, { "group": "A", "variable": "v8", "value": "20" }, { "group": "A", "variable": "v9", "value": "99" }, { "group": "A", "variable": "v10", "value": "66" }, { "group": "B", "variable": "v1", "value": "37" }, { "group": "B", "variable": "v2", "value": "50" }, { "group": "B", "variable": "v3", "value": "81" }, { "group": "B", "variable": "v4", "value": "79" }, { "group": "B", "variable": "v5", "value": "84" }, { "group": "B", "variable": "v6", "value": "91" }, { "group": "B", "variable": "v7", "value": "82" }, { "group": "B", "variable": "v8", "value": "89" }, { "group": "B", "variable": "v9", "value": "6" }, { "group": "B", "variable": "v10", "value": "67" }, { "group": "C", "variable": "v1", "value": "96" }, { "group": "C", "variable": "v2", "value": "13" }, { "group": "C", "variable": "v3", "value": "98" }, { "group": "C", "variable": "v4", "value": "10" }, { "group": "C", "variable": "v5", "value": "86" }, { "group": "C", "variable": "v6", "value": "23" }, { "group": "C", "variable": "v7", "value": "74" }, { "group": "C", "variable": "v8", "value": "47" }, { "group": "C", "variable": "v9", "value": "73" }, { "group": "C", "variable": "v10", "value": "40" }, { "group": "D", "variable": "v1", "value": "75" }, { "group": "D", "variable": "v2", "value": "18" }, { "group": "D", "variable": "v3", "value": "92" }, { "group": "D", "variable": "v4", "value": "43" }, { "group": "D", "variable": "v5", "value": "16" }, { "group": "D", "variable": "v6", "value": "27" }, { "group": "D", "variable": "v7", "value": "76" }, { "group": "D", "variable": "v8", "value": "24" }, { "group": "D", "variable": "v9", "value": "1" }, { "group": "D", "variable": "v10", "value": "87" }, { "group": "E", "variable": "v1", "value": "44" }, { "group": "E", "variable": "v2", "value": "29" }, { "group": "E", "variable": "v3", "value": "58" }, { "group": "E", "variable": "v4", "value": "55" }, { "group": "E", "variable": "v5", "value": "65" }, { "group": "E", "variable": "v6", "value": "56" }, { "group": "E", "variable": "v7", "value": "9" }, { "group": "E", "variable": "v8", "value": "78" }, { "group": "E", "variable": "v9", "value": "49" }, { "group": "E", "variable": "v10", "value": "36" }, { "group": "F", "variable": "v1", "value": "35" }, { "group": "F", "variable": "v2", "value": "80" }, { "group": "F", "variable": "v3", "value": "8" }, { "group": "F", "variable": "v4", "value": "46" }, { "group": "F", "variable": "v5", "value": "48" }, { "group": "F", "variable": "v6", "value": "100" }, { "group": "F", "variable": "v7", "value": "17" }, { "group": "F", "variable": "v8", "value": "41" }, { "group": "F", "variable": "v9", "value": "33" }, { "group": "F", "variable": "v10", "value": "11" }, { "group": "G", "variable": "v1", "value": "77" }, { "group": "G", "variable": "v2", "value": "62" }, { "group": "G", "variable": "v3", "value": "94" }, { "group": "G", "variable": "v4", "value": "15" }, { "group": "G", "variable": "v5", "value": "69" }, { "group": "G", "variable": "v6", "value": "63" }, { "group": "G", "variable": "v7", "value": "61" }, { "group": "G", "variable": "v8", "value": "54" }, { "group": "G", "variable": "v9", "value": "38" }, { "group": "G", "variable": "v10", "value": "93" }, { "group": "H", "variable": "v1", "value": "39" }, { "group": "H", "variable": "v2", "value": "26" }, { "group": "H", "variable": "v3", "value": "90" }, { "group": "H", "variable": "v4", "value": "83" }, { "group": "H", "variable": "v5", "value": "31" }, { "group": "H", "variable": "v6", "value": "2" }, { "group": "H", "variable": "v7", "value": "51" }, { "group": "H", "variable": "v8", "value": "28" }, { "group": "H", "variable": "v9", "value": "42" }, { "group": "H", "variable": "v10", "value": "7" }, { "group": "I", "variable": "v1", "value": "5" }, { "group": "I", "variable": "v2", "value": "60" }, { "group": "I", "variable": "v3", "value": "21" }, { "group": "I", "variable": "v4", "value": "25" }, { "group": "I", "variable": "v5", "value": "3" }, { "group": "I", "variable": "v6", "value": "70" }, { "group": "I", "variable": "v7", "value": "34" }, { "group": "I", "variable": "v8", "value": "68" }, { "group": "I", "variable": "v9", "value": "57" }, { "group": "I", "variable": "v10", "value": "32" }, { "group": "J", "variable": "v1", "value": "19" }, { "group": "J", "variable": "v2", "value": "85" }, { "group": "J", "variable": "v3", "value": "53" }, { "group": "J", "variable": "v4", "value": "45" }, { "group": "J", "variable": "v5", "value": "71" }, { "group": "J", "variable": "v6", "value": "64" }, { "group": "J", "variable": "v7", "value": "4" }, { "group": "J", "variable": "v8", "value": "12" }, { "group": "J", "variable": "v9", "value": "97" }, { "group": "J", "variable": "v10", "value": "72" }];
 
-      //==================================================
-      var viewerWidth = $(document).width();
-      var viewerHeight = $(document).height();
-      var viewerPosTop = 200;
-      var legendHeight = 70;
-      var legendElementWidth = CELL_SIZE * 4.5;
-      var svg;
+      console.log(data);
 
-      //==================================================
+      // set the dimensions and margins of the graph
+      var margin = { top: 120, right: 25, bottom: 30, left: 100 };
+      var width = window.innerWidth / 1.5;
+      var height = window.innerHeight / 1.5;
 
-      var arr = exams.data;
-      var row_number = arr.length;
-      var col_number = arr[0] ? arr[0].length : 0;
-
-      svg = d3.select(heatmapId)
+      // append the svg object to the body of the page
+      var svg = d3.select("#exam-heatmap")
         .append("svg")
-        .attr("width", viewerWidth - 100)
-        .attr("height", ((row_number * 100) + viewerPosTop) + "px")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform", "translate(" + window.innerWidth / 3.5 + "," + window.innerHeight / 4 + ")scale(" + window.innerWidth / 1800 + ")");
+        .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-      svg.append('defs')
-        .append('pattern')
-        .attr('id', 'diagonalHatch')
-        .attr('patternUnits', 'userSpaceOnUse')
-        .attr('width', 4)
-        .attr('height', 4)
-        .append('path')
-        .attr('d', 'M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2')
-        .attr('stroke', '#FFF')
-        .attr('stroke-width', 1);
+      // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+      var myGroups = fullData.x;
+      var myVars = fullData.y;
 
-      var rowSortOrder = false;
-      var colSortOrder = false;
+      // Build X scales and axis:
+      var x = d3.scaleBand()
+        .range([0, width])
+        .domain(myGroups)
+        .padding(0.05);
+      svg.append("g")
+        .style("font-size", 15)
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).tickSize(0))
+        .select(".domain").remove()
 
-      var rowLabels = svg.append("g")
-        .attr("class", "rowLabels")
-        .selectAll(".rowLabel")
-        .data(exams.index)
-        .enter().append("text")
-        .text(function (d) {
-          return d.count > 1 ? d.join("/") : d;
-        })
-        .attr("x", 0)
-        .attr("y", function (d, i) {
-          return (i * CELL_SIZE);
-        })
-        .style("text-anchor", "end")
-        .attr("transform", function (d, i) {
-          return "translate(-3," + CELL_SIZE / 1.5 + ")";
-        })
-        .attr("class", "rowLabel mono")
-        .attr("id", function (d, i) {
-          return "rowLabel_" + i;
-        })
-        .on('mouseover', function (d, i) {
-          d3.select('#rowLabel_' + i).classed("hover", true);
-        })
-        .on('mouseout', function (d, i) {
-          d3.select('#rowLabel_' + i).classed("hover", false);
-        })
-        .on("click", function (d, i) {
-          rowSortOrder = !rowSortOrder;
-          _sortByValues("r", i, rowSortOrder);
-          d3.select("#order").property("selectedIndex", 0);
-        });
+      // Build Y scales and axis:
+      var y = d3.scaleBand()
+        .range([height, 0])
+        .domain(myVars)
+        .padding(0.05);
+      svg.append("g")
+        .style("font-size", 15)
+        .call(d3.axisLeft(y).tickSize(0))
+        .select(".domain").remove()
 
-      var colLabels = svg.append("g")
-        .attr("class", "colLabels")
-        .selectAll(".colLabel")
-        .data(exams.columns)
-        .enter().append("text")
-        .text(function (d) {
-          d.shift();
-          return d.count > 1 ? d.reverse().join("/") : d.reverse();
-        })
-        .attr("x", 0)
-        .attr("y", function (d, i) {
-          return (i * CELL_SIZE);
-        })
-        .style("text-anchor", "left")
-        .attr("transform", function (d, i) {
-          return "translate(" + CELL_SIZE / 2 + ", -3) rotate(-90) rotate(45, 0, " + (i * CELL_SIZE) + ")";
-        })
-        .attr("class", "colLabel mono")
-        .attr("style", "cursor:pointer")
-        .attr("id", function (d, i) {
-          return "colLabel_" + i;
-        })
-        .on('mouseover', function (d, i) {
-          d3.select('#colLabel_' + i).classed("hover", true);
-        })
-        .on('mouseout', function (d, i) {
-          d3.select('#colLabel_' + i).classed("hover", false);
-        })
-        .on("click", function (d, i) {
-          colSortOrder = !colSortOrder;
-          _sortByValues("c", i, colSortOrder);
-          d3.select("#order").property("selectedIndex", 0);
-        });
+      // Build color scale
+      var myColor = d3.scaleSequential()
+        .interpolator(d3.interpolateInferno)
+        .domain([1, 100])
 
-      //Index to lines to d3 
-      var lineIndexOne = -1;
-      var lineIndexTwo = -1;
-      var lineIndexThree = -1;
+      // create a tooltip
+      var tooltip = d3.select("#exam-heatmap")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
 
-      var row = svg.selectAll(".row")
-        .data(exams.data)
-        .enter().append("g")
-        .attr("id", function (d) {
-          return d.idx;
-        })
-        .attr("class", "row");
-
-      var j = 0;
-      var heatMap = row.selectAll(".cell")
-        .data(function (d) {
-          j++;
-          return d;
-        })
-        .enter().append("svg:rect")
-        .attr("x", function (d, i) {
-          return i * CELL_SIZE;
-        })
-        .attr("y", function (d, i) {
-          if (i == 0) {
-            lineIndexOne++;
-          }
-          return lineIndexOne * CELL_SIZE;
-        })
+      // add the squares
+      svg.selectAll()
+        .data(data, function (d) { return d.group + ':' + d.variable; })
+        .enter()
+        .append("rect")
+        .attr("x", function (d) { return x(d.group) })
+        .attr("y", function (d) { return y(d.variable) })
         .attr("rx", 4)
-        .attr("ry", 4)
-        .attr("class", function (d, i) {
-          if (i == 0) {
-            lineIndexTwo++;
-          }
-          return "cell bordered cr" + lineIndexTwo + " cc" + i;
-        })
-        .attr("row", function (d, i) {
-          if (i == 0) {
-            lineIndexThree++;
-          }
-          return lineIndexThree;
-        })
-        .attr("col", function (d, i) {
-          return i;
-        })
-        .attr("width", CELL_SIZE)
-        .attr("height", CELL_SIZE)
-        .style("fill", function (d) {
-          if (d != null) return ExamStatusHistoryService.getStatusColor(d);
-          else return "url(#diagonalHatch)";
-        })
-        .on('mouseover', function (d, i) {
-          var j = d3.select(this).attr("row");
-          d3.select('#colLabel_' + i).classed("hover", true);
-          d3.select('#rowLabel_' + j).classed("hover", true);
-          tooltip.style("visibility", "visible");
-        })
-        .on('mouseout', function (d, i) {
-          var j = d3.select(this).attr("row");
-          d3.select('#colLabel_' + i).classed("hover", false);
-          d3.select('#rowLabel_' + j).classed("hover", false);
-          tooltip.style("visibility", "hidden");
-        })
-        .on("mousemove", function (d, i) {
-          tooltip.style("top", (d3.event.pageY - 320) + "px").style("left", (d3.event.pageX - 75) + "px");
-        });
+        .attr("ry", 10)
+        .attr("width", x.bandwidth())
+        .attr("height", y.bandwidth())
+        .style("fill", function (d) { return myColor(d.value) })
+        .style("stroke-width", 4)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
 
-      var legend = svg.append("g")
-        .attr("class", "legend")
-        .attr("transform", "translate(-45,-300)")
-        .selectAll(".legendElement")
-        .data(self.labels)
-        .enter().append("g")
-        .attr("class", "legendElement");
+    }
 
-      legend.append("svg:rect")
-        .attr("x", function (d, i) {
-          return legendElementWidth * i;
-        })
-        .attr("y", legendHeight)
-        .attr("class", "cellLegend bordered")
-        .attr("width", legendElementWidth)
-        .attr("height", CELL_SIZE / 2)
-        .style("fill", function (d, i) {
-          return self.colors[i];
-        });
+    function create(resp) {
+      let ob = function (column, row, value) { return { group: column, variable: row, value: value } };
+      let exams = resp.columns.map(column => column[1])
+      let values = [];
 
-      legend.append("text")
-        .attr("class", "mono legendElement")
-        .text(function (d) {
-          return d;
-        })
-        .attr("x", function (d, i) {
-          return legendElementWidth * i;
-        })
-        .attr("y", legendHeight + CELL_SIZE);
-
-      function _sortByValues(rORc, i, sortOrder) {
-        var t = svg.transition().duration(1000);
-        var values = [];
-        var sorted;
-        d3.selectAll(".c" + rORc + i)
-          .filter(function (d) {
-            if (d != null) values.push(d);
-            else values.push(-999);
-          });
-
-        if (rORc == "r") {
-          sorted = d3.range(col_number).sort(function (a, b) {
-            if (sortOrder) {
-              return values[b] - values[a];
-            } else {
-              return values[a] - values[b];
-            }
-          });
-          t.selectAll(".cell")
-            .attr("x", function (d) {
-              var col = parseInt(d3.select(this).attr("col"));
-              return sorted.indexOf(col) * CELL_SIZE;
-            });
-          t.selectAll(".colLabel")
-            .attr("y", function (d, i) {
-              return sorted.indexOf(i) * CELL_SIZE;
-            })
-            .attr("transform", function (d, i) {
-              return "translate(" + CELL_SIZE / 2 + ", -3) rotate(-90) rotate(45, 0, " + (sorted.indexOf(i) * CELL_SIZE) + ")";
-            });
-        } else { // sort on rows
-          sorted = d3.range(row_number).sort(function (a, b) {
-            if (sortOrder) {
-              return values[b] - values[a];
-            } else {
-              return values[a] - values[b];
-            }
-          });
-          t.selectAll(".cell")
-            .attr("y", function (d) {
-              var row = parseInt(d3.select(this).attr("row"));
-              return sorted.indexOf(row) * CELL_SIZE;
-            });
-          t.selectAll(".rowLabel")
-            .attr("y", function (d, i) {
-              return sorted.indexOf(i) * CELL_SIZE;
-            })
-            .attr("transform", function (d, i) {
-              return "translate(-3," + CELL_SIZE / 1.5 + ")";
-            });
-        }
-      }
-
-      d3.select("#order").on("change", function () {
-        var newOrder = d3.select("#order").property("value");
-        _changeOrder(newOrder, heatmapId);
+      resp.index.forEach(participant => {
+        values = values.concat(exams.map(exam => {
+          return new ob(exam, participant, getRandomInt(0, 1) * 90)
+        }));
       });
 
-      d3.select("#palette")
-        .on("keyup", function () {
-          var newPalette = d3.select("#palette").property("value");
-          if (newPalette != null)
-            changePalette(newPalette, heatmapId);
-        })
-        .on("change", function () {
-          var newPalette = d3.select("#palette").property("value");
-          changePalette(newPalette, heatmapId);
-        });
+      function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+
+      return { x: exams, y: resp.index, values: values }
     }
 
-    function _changeOrder(newOrder, heatmapId) {
-      var svg = d3.select(heatmapId);
-      var t = svg.transition().duration(1000);
-      if (newOrder == "sortinit_col") {
-        t.selectAll(".cell")
-          .attr("x", function (d) {
-            var col = parseInt(d3.select(this).attr("col"));
-            return col * CELL_SIZE;
-          });
-        t.selectAll(".colLabel")
-          .attr("y", function (d, i) {
-            return i * CELL_SIZE;
-          })
-          .attr("transform", function (d, i) {
-            return "translate(" + CELL_SIZE / 2 + ", -3) rotate(-90) rotate(45, 0, " + (i * CELL_SIZE) + ")";
-          });
-      } else if (newOrder == "sortinit_row") {
-        t.selectAll(".cell")
-          .attr("y", function (d) {
-            var row = parseInt(d3.select(this).attr("row"));
-            return row * CELL_SIZE;
-          });
-        t.selectAll(".rowLabel")
-          .attr("y", function (d, i) {
-            return i * CELL_SIZE;
-          })
-          .attr("transform", function (d, i) {
-            return "translate(-3," + CELL_SIZE / 1.5 + ")";
-          });
-      } else if (newOrder == "sortinit_col_row") {
-        t.selectAll(".cell")
-          .attr("x", function (d) {
-            var col = parseInt(d3.select(this).attr("col"));
-            return col * CELL_SIZE;
-          })
-          .attr("y", function (d) {
-            var row = parseInt(d3.select(this).attr("row"));
-            return row * CELL_SIZE;
-          });
-        t.selectAll(".colLabel")
-          .attr("y", function (d, i) {
-            return i * CELL_SIZE;
-          })
-          .attr("transform", function (d, i) {
-            return "translate(" + CELL_SIZE / 2 + ", -3) rotate(-90) rotate(45, 0, " + (i * CELL_SIZE) + ")";
-          });
-        t.selectAll(".rowLabel")
-          .attr("y", function (d, i) {
-            return i * CELL_SIZE;
-          })
-          .attr("transform", function (d, i) {
-            return "translate(-3," + CELL_SIZE / 1.5 + ")";
-          });
-      }
-    }
 
     function _setCenter(acronym) {
       self.selectedCenter = self.centers.find(function (center) {
