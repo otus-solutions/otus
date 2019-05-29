daf179eba0bf02a8a130ca3a389119ca8b1c480e
Adonis Garcia <adonis.garcia.adg@gmail.com>
2019-05-29 14:00:39 -0300

    OTUS-545 teste de unidade dashboard-home-display-component-spec.js

---
+++ otus/app/ux-component/dashboard-home-display/dashboard-home-display-controller.js
@@ -0,0 +1,106 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('otusjs.otus.uxComponent')
+    .controller('otusDashboardHomeDisplayCtrl', Controller);
+
+  Controller.$inject = [
+    'otusjs.otus.dashboard.core.EventService',
+    'otusjs.otus.dashboard.core.ContextService',
+    'otusjs.application.state.ApplicationStateService',
+    'otusjs.user.business.UserAccessPermissionService',
+    'otusjs.laboratory.business.participant.ParticipantLaboratoryService'
+  ];
+
+  function Controller(EventService, ContextService, ApplicationStateService, UserAccessPermissionService, ParticipantLaboratoryService) {
+    var self = this;
+    self.participantsReady = false;
+    self.laboratoryChecking;
+    self.userAccessToLaboratory;
+
+    /* Lifecycle hooks */
+    self.$onInit = onInit;
+    /* Public methods */
+    self.setFocus = setFocus;
+    self.sendingExam = sendingExam;
+    self.examsDashboard = examsDashboard;
+    self.startMonitoring = startMonitoring;
+    self.laboratoryMonitoring = laboratoryMonitoring;
+    self.sampleTransportDashboard = sampleTransportDashboard;
+    self.managerParticipantsDashboard = managerParticipantsDashboard;
+    self.activateActivityFlagsReport = activateActivityFlagsReport;
+    self.laboratoryActivityFlagsReport = laboratoryActivityFlagsReport;
+
+    function onInit() {
+      _loadLoggedUser();
+      EventService.onLogin(_loadLoggedUser);
+      _getCheckingExist();
+      _checkingLaboratoryPermission();
+    }
+
+    function startMonitoring() {
+      ApplicationStateService.activateMonitoring();
+    }
+
+    function setFocus() {
+      setTimeout(function () {
+        document.querySelector('#participantSearchAutoCompleteId').focus();
+      }, 0);
+    }
+
+    function sampleTransportDashboard() {
+      ApplicationStateService.activateSampleTransportation();
+    }
+
+    function managerParticipantsDashboard() {
+      ApplicationStateService.activateParticipantsList();
+    }
+
+    function examsDashboard() {
+      ApplicationStateService.activateExamsLotsManagerList();
+    }
+
+    function activateActivityFlagsReport() {
+      ApplicationStateService.activateActivityFlagsReport();
+    }
+
+    function laboratoryActivityFlagsReport() {
+      ApplicationStateService.laboratoryActivityFlagsReport();
+    }
+
+    function sendingExam() {
+      ApplicationStateService.activateExamSending();
+    }
+
+    function laboratoryMonitoring() {
+      ApplicationStateService.activateLaboratoryMonitoring();
+    }
+
+    function _loadLoggedUser(userData) {
+      if (userData) {
+        self.loggedUser = userData;
+      } else {
+        ContextService
+          .getLoggedUser()
+          .then(function (userData) {
+            self.loggedUser = userData;
+          });
+      }
+    }
+
+    function _getCheckingExist() {
+      ParticipantLaboratoryService.getCheckingExist()
+        .then(function (response) {
+          self.laboratoryChecking = response;
+        });
+    }
+
+    function _checkingLaboratoryPermission() {
+      UserAccessPermissionService.getCheckingLaboratoryPermission().then(response => {
+        self.userAccessToLaboratory = response;
+      });
+    }
+
+  }
+}());
---
+++ otus/tests/unit/app/ux-component/dashboard-home-display/dashboard-home-display-component-spec.js
@@ -0,0 +1,137 @@
+describe('otusDashboardHomeDisplayCtrl_UnitTest_Suite', function () {
+  var Mock = {};
+  var Injections = [];
+  var ctrl;
+
+  beforeEach(function () {
+    mockInjections();
+
+    angular.mock.module('otusjs.otus.uxComponent');
+    angular.mock.module('otusjs.otus.dashboard');
+    angular.mock.module(function ($provide) {
+      $provide.value('otusjs.application.state.ApplicationStateService', Mock.ApplicationStateService);
+    });
+    angular.mock.module('otusjs.laboratory');
+    angular.mock.module('otusjs.otus.laboratory');
+    angular.mock.module('otusjs.user');
+
+    angular.mock.inject(function (_$controller_, _$injector_) {
+      Injections.EventService = _$injector_.get('otusjs.otus.dashboard.core.EventService');
+      Injections.ContextService = _$injector_.get('otusjs.otus.dashboard.core.ContextService');
+      Injections.ApplicationStateService = _$injector_.get('otusjs.application.state.ApplicationStateService');
+      Injections.UserAccessPermissionService = _$injector_.get('otusjs.user.business.UserAccessPermissionService');
+      Injections.ParticipantLaboratoryService = _$injector_.get('otusjs.laboratory.business.participant.ParticipantLaboratoryService');
+
+      ctrl = _$controller_('otusDashboardHomeDisplayCtrl', Injections);
+
+      spyOn(Injections.EventService, "onLogin").and.callThrough();
+      spyOn(Injections.ContextService,"getLoggedUser").and.callThrough();
+      spyOn(Injections.ParticipantLaboratoryService,"getCheckingExist").and.callThrough();
+      spyOn(Injections.UserAccessPermissionService,"getCheckingLaboratoryPermission").and.callThrough();
+      spyOn(Injections.ApplicationStateService, "activateMonitoring");
+      spyOn(Injections.ApplicationStateService, "activateSampleTransportation");
+      spyOn(Injections.ApplicationStateService, "activateParticipantsList");
+      spyOn(Injections.ApplicationStateService, "activateExamsLotsManagerList");
+      spyOn(Injections.ApplicationStateService, "activateActivityFlagsReport");
+      spyOn(Injections.ApplicationStateService, "laboratoryActivityFlagsReport");
+      spyOn(Injections.ApplicationStateService, "activateExamSending");
+      spyOn(Injections.ApplicationStateService, "activateLaboratoryMonitoring");
+      spyOn(ctrl,"setFocus");
+    });
+  });
+
+  it('ctrlExistence_check', function () {
+    expect(ctrl).toBeDefined();
+  });
+
+  it('ctrlMethodsExistence_check', function () {
+    expect(ctrl.$onInit).toBeDefined();
+    expect(ctrl.setFocus).toBeDefined();
+    expect(ctrl.sendingExam).toBeDefined();
+    expect(ctrl.examsDashboard).toBeDefined();
+    expect(ctrl.startMonitoring).toBeDefined();
+    expect(ctrl.laboratoryMonitoring).toBeDefined();
+    expect(ctrl.sampleTransportDashboard).toBeDefined();
+    expect(ctrl.managerParticipantsDashboard).toBeDefined();
+    expect(ctrl.activateActivityFlagsReport).toBeDefined();
+    expect(ctrl.laboratoryActivityFlagsReport).toBeDefined();
+  });
+
+  it('onInit_method_should_evoke_internalMethods', function () {
+    ctrl.$onInit();
+    expect(Injections.EventService.onLogin).toHaveBeenCalledTimes(1);
+    expect(Injections.ContextService.getLoggedUser).toHaveBeenCalledTimes(1);
+    expect(Injections.ParticipantLaboratoryService.getCheckingExist).toHaveBeenCalledTimes(1);
+    expect(Injections.UserAccessPermissionService.getCheckingLaboratoryPermission).toHaveBeenCalledTimes(1);
+  });
+
+  it('startMonitoring_method_should_evoke_internalMethods', function () {
+    ctrl.startMonitoring();
+    expect(Injections.ApplicationStateService.activateMonitoring).toHaveBeenCalledTimes(1);
+  });
+
+  it('sampleTransportDashboard_method_should_evoke_internalMethods', function () {
+    ctrl.sampleTransportDashboard();
+    expect(Injections.ApplicationStateService.activateSampleTransportation).toHaveBeenCalledTimes(1);
+  });
+
+  it('managerParticipantsDashboard_method_should_evoke_internalMethods', function () {
+    ctrl.managerParticipantsDashboard();
+    expect(Injections.ApplicationStateService.activateParticipantsList).toHaveBeenCalledTimes(1);
+  });
+
+  it('examsDashboard_method_should_evoke_internalMethods', function () {
+    ctrl.examsDashboard();
+    expect(Injections.ApplicationStateService.activateExamsLotsManagerList).toHaveBeenCalledTimes(1);
+  });
+
+  it('activateActivityFlagsReport_method_should_evoke_internalMethods', function () {
+    ctrl.activateActivityFlagsReport();
+    expect(Injections.ApplicationStateService.activateActivityFlagsReport).toHaveBeenCalledTimes(1);
+  });
+
+  it('laboratoryActivityFlagsReport_method_should_evoke_internalMethods', function () {
+    ctrl.laboratoryActivityFlagsReport();
+    expect(Injections.ApplicationStateService.laboratoryActivityFlagsReport).toHaveBeenCalledTimes(1);
+  });
+
+  it('sendingExam_method_should_evoke_internalMethods', function () {
+    ctrl.sendingExam();
+    expect(Injections.ApplicationStateService.activateExamSending).toHaveBeenCalledTimes(1);
+  });
+
+  it('laboratoryMonitoring_method_should_evoke_internalMethods', function () {
+    ctrl.laboratoryMonitoring();
+    expect(Injections.ApplicationStateService.activateLaboratoryMonitoring).toHaveBeenCalledTimes(1);
+  });
+
+  function mockInjections() {
+    Mock.ApplicationStateService = {
+      activateMonitoring: function () {
+        return Promise.resolve();
+      },
+      activateSampleTransportation: function () {
+        return Promise.resolve();
+      },
+      activateParticipantsList: function () {
+        return Promise.resolve();
+      },
+      activateExamsLotsManagerList: function () {
+        return Promise.resolve();
+      },
+      activateActivityFlagsReport: function () {
+        return Promise.resolve();
+      },
+      laboratoryActivityFlagsReport: function () {
+        return Promise.resolve();
+      },
+      activateExamSending: function () {
+        return Promise.resolve();
+      },
+      activateLaboratoryMonitoring: function () {
+        return Promise.resolve();
+      }
+    }
+  }
+
+});
