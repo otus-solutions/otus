0568d5ad2333a102c6eaeed15a7e2d78b9319d57
Breno Scheffer <brenoscheffer@gmail.com>
2019-08-01 17:02:54 -0300

    OTUS-584 Inicialização fake do rest de variáveis estáticas para realização do QA do otus-preview-js

--- otus/app/deploy/rest/otus-api/static-variable/static-variable-rest-service.js
+++ otus/app/deploy/rest/otus-api/static-variable/static-variable-rest-service.js
@@ -19,6 +19,31 @@
 
     function initialize() {
       _rest = OtusRestResourceService.getStaticVariableResource();
+
+      fakeInitialize();
+    }
+
+    function fakeInitialize() {
+      _rest = {
+        getStaticVariableList: function () {
+          return Promise.resolve({
+            data: {
+              variables:[
+                {
+                  name: "var1",
+                  sending: "onda 1",
+                  value: 0
+                },
+                {
+                  name: "var2",
+                  sending: "onda 2",
+                  value: "30Kg"
+                }
+              ]
+            }
+          });
+        }
+      }
     }
 
     function getParticipantStaticVariable(variableRequest) {
@@ -28,7 +53,7 @@
 
       return _rest
         .getStaticVariableList({request:variableRequest})
-        .$promise
+        // .$promise
         .then(function (response) {
           if (response.data) {
             return response.data;
