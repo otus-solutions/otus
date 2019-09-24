b760c4fa3e8ad96ac0ec5178695827ac87301802
Fabiano Ramires <fdrtec@gmail.com>
2019-09-24 14:29:46 -0300

    OTUS-606 implementação do terceiro state do botão e o alert para o mesmo

--- otus/app/ux-component/report/activity-report/activity-report-component.js
+++ otus/app/ux-component/report/activity-report/activity-report-component.js
@@ -15,31 +15,19 @@
   Controller.$inject = [
     'otusjs.activity.business.ParticipantActivityService',
     'otusjs.report.business.ParticipantReportWidgetFactory',
-    'otusjs.deploy.LoadingScreenService'
+    'otusjs.deploy.LoadingScreenService',
+    '$mdDialog'
   ];
 
-  function Controller(ParticipantActivityService, ParticipantReportWidgetFactory, LoadingScreenService){
+  function Controller(ParticipantActivityService, ParticipantReportWidgetFactory, LoadingScreenService, $mdDialog){
     var self = this;
 
     self.reloadActivityReport = reloadActivityReport;
     self.generateActivityReport = generateActivityReport;
+    self.pendingActivityReport = pendingActivityReport;
 
-
-    // function reloadActivityReport() {
-    //   let selectedActivityID = ParticipantActivityService.getSelectedActivities().list()[0].getID();
-    //
-    //   self.activityReportReady = false;
-    //   //TODO: estamos buscando um relatório de exame para simular a replicação do mecanismo, modificar metodo para buscar relatório pelo ID da atividade
-    //   ParticipantReportWidgetFactory.getParticipantReportList(self.selectedParticipant)
-    //     .then(function (reports) {
-    //       self.report = reports[0];
-    //       self.report.getReportTemplate();
-    //       self.activityReportReady = true;
-    //     })
-    //     .catch(function () {
-    //       self.activityReportReady = true;
-    //     });
-    // }
+    self.activityReportInfo = false;
+    self.activityReportReady = false;
 
     function reloadActivityReport() {
       let selectedActivityID = ParticipantActivityService.getSelectedActivities().list()[0].getID();
@@ -53,21 +41,30 @@
           self.activityReportReady = true;
         })
         .catch(function () {
-          self.activityReportReady = true;
+          self.activityReportReady = false;
+          self.activityReportInfo = true;
         });
     }
 
-
-
-
-
     function generateActivityReport(report) {
-      console.log(report)
-      console.log(report.compiledTemplate);
+      //console.log(report)
+      //console.log(report.compiledTemplate);
       LoadingScreenService.changeMessage(report.getLoadingMessage());
       LoadingScreenService.start();
       report.generateReport(LoadingScreenService.finish);
-      // self.activityReportReady = false;
+      self.activityReportReady = false;
+    }
+
+    function pendingActivityReport() {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title('Relatório Incompleto: Pendências Detectadas')
+          .textContent('Para visualizar/imprimir o relatório será necessário validar os itens abaixo.')
+          .ariaLabel('Alert: Relatório incompleto')
+          .ok('Voltar')
+      );
     }
   }
 
