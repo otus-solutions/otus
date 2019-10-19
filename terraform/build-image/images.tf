c723c9fbc838c077f967d84991d3e23923b3d75c
Fernando Souza <fernandosdba@gmail.com>
2019-10-18 21:00:42 -0300

    OTUS-605 Alteração de declaração de variaveis

--- terraform/build-image/images.tf
+++ terraform/build-image/images.tf
@@ -1,25 +1,36 @@
 ###############################################
 ###               Variables                 ###
 ###############################################
-
-variable "otus-frontend" {
-  type = "map"
-  default = {
-    "name" = "otus-frontend"
-    "directory" = "otus"
-    "source" = "/source"
-  }
+variable "otus-frontend-name" {
+  default = "otus-frontend"  
 }
 
+variable "otus-frontend-directory" {
+  default = "otus"  
+}
+
+variable "otus-frontend-source" {
+  default = "/source"  
+}
+
+variable "otus-frontend-npmbuild" {
+  default = "install"  
+}
 ###############################################
 ###  OTUS : Build Image Front-End           ###
 ###############################################
 
-resource "null_resource" "otus-frontend" {
+resource "null_resource" "otus-frontend-build" {
   provisioner "local-exec" {
-    command = "cd ${var.otus-frontend["directory"]}/${var.otus-frontend["source"]} && npm install"
+    working_dir = "otus/source"
+    command = "npm ${var.otus-frontend-npmbuild}"
   }
+} 
+ 
+resource "null_resource" "otus-frontend" {
+depends_on = [null_resource.otus-frontend-build]  
   provisioner "local-exec" {
-    command = "sudo docker build -t ${var.otus-frontend["name"]} ${var.otus-frontend["directory"]}"
+    working_dir = "otus"
+    command = "docker build -t ${var.otus-frontend-name} ."
   }
 }
