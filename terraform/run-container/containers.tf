2f0050baeeab4cdd7d0d3e27dee5b7026969fc00
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2019-10-25 17:55:42 -0300

    Parametro para API_URL

--- terraform/run-container/containers.tf
+++ terraform/run-container/containers.tf
@@ -6,6 +6,10 @@ variable "otus-frontend-port"{
   default = 51001
 }
 
+variable "otus-frontend-apiurl"{
+  default = "http://otus-api:8080"
+}
+
 variable "otus-frontend-version"{
   default = "latest"
 }
@@ -17,6 +21,7 @@ resource "docker_image" "otus-frontend" {
 resource "docker_container" "otus-frontend" {
   name = "otus-frontend"
   image = "${docker_image.otus-frontend.name}"
+  env = ["API_URL=${var.otus-frontend-apiurl}"]
   ports {
 	internal = 80
 	external = "${var.otus-frontend-port}"
