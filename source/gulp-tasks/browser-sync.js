ef9700394634b8f2cd2d380ac38d71973f5bdd87
Breno Scheffer <brenoscheffer@gmail.com>
2019-10-29 20:38:08 -0300

    Browser-sync adaptado para nova hierarquia de pastas do projeto

--- source/gulp-tasks/browser-sync.js
+++ source/gulp-tasks/browser-sync.js
@@ -1,28 +1,30 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var browserSyncSpa = require('browser-sync-middleware-spa');
-
+var minimist = require('minimist');
+​
 gulp.task('browser-sync', function() {
+  console.log(process.argv);
+  var index = process.argv.indexOf("--api-url");
+  var apiurl = process.argv[index + 1];
   browserSync.init({
     server: {
       open: 'external',
-      baseDir: '../',
+      baseDir: ['./app', './'],
       middleware: [
-        //browserSyncSpa(/^[^\.]+$/, baseDir),
-
         function(req, res, next) {
           res.setHeader('Access-Control-Allow-Origin', '*');
           res.setHeader('Access-Control-Allow-Headers', '*');
+          res.setHeader('Backend-Address', apiurl);
           next();
         }
       ]
     },
-    startPath: 'otus/app'
   });
-
-  gulp.watch([
-    'app/**/*.html',
-    'app/**/*.js',
-    '../app/**/*.css'
-  ]).on('change', browserSync.reload);
+​
+    gulp.watch([
+      'app/**/*.html',
+      'app/**/*.js',
+      '../app/**/*.css'
+    ]).on('change', browserSync.reload);
 });
