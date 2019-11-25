4ced3b6b2f0ae98a0a08e2e26eff01b4f2822a0a
emanoelvianna <emanoel.vianna@outlook.com>
2019-11-25 13:49:40 -0300

    OTUS-619 - modificações para obter dados do storage

--- source/app/module/activity/business/participant-activity-service.js
+++ source/app/module/activity/business/participant-activity-service.js
@@ -1,197 +1,201 @@
 (function () {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('otusjs.activity.business')
-        .service('otusjs.activity.business.ParticipantActivityService', Service);
+	angular
+		.module('otusjs.activity.business')
+		.service('otusjs.activity.business.ParticipantActivityService', Service);
 
-    Service.$inject = [
-        'otusjs.activity.core.ModuleService',
-        'otusjs.activity.core.ContextService',
-        'otusjs.activity.repository.ActivityRepositoryService',
-        'otusjs.activity.repository.UserRepositoryService',
-        'otusjs.activity.business.ActivityDtoFactory',
-        'otusjs.application.state.ApplicationStateService'
-    ];
+	Service.$inject = [
+		'otusjs.activity.core.ModuleService',
+		'otusjs.activity.core.ContextService',
+		'otusjs.activity.repository.ActivityRepositoryService',
+		'otusjs.activity.repository.UserRepositoryService',
+		'otusjs.activity.business.ActivityDtoFactory',
+		'otusjs.application.state.ApplicationStateService',
+		'SurveyFactory'
+	];
 
-    function Service(ModuleService, ContextService, ActivityRepositoryService, UserRepositoryService, ActivityDtoFactory,ApplicationStateService) {
-        var self = this;
-        var _paperActivityCheckerData = null;
-        self.activityConfigurations = new Object();
-        self.activities = [];
-
-
-        /* Public methods */
-        self.initializePaperActivityData = initializePaperActivityData;
-        self.add = add;
-        self.listAll = listAll;
-        self.listAllCategories = listAllCategories;
-        self.listAvailables = listAvailables;
-        self.selectActivities = selectActivities;
-        self.getSelectedActivities = getSelectedActivities;
-        self.getSelectedParticipant = getSelectedParticipant;
-        self.listActivityCheckers = listActivityCheckers;
-        self.getById = getById;
-        self.setActivitiesSelection = setActivitiesSelection;
-        self.getActivitiesSelection = getActivitiesSelection;
-        self.updateCheckerActivity = updateCheckerActivity;
-
-        self.configurationStructure = configurationStructure;
-        self.addActivityRevision = addActivityRevision;
-        self.getActivityRevisions = getActivityRevisions;
-
-        self.createActivityDto = createActivityDto;
-        self.saveActivities = saveActivities;
+	function Service(ModuleService, ContextService, ActivityRepositoryService, UserRepositoryService, ActivityDtoFactory, ApplicationStateService, SurveyFactory) {
+		var self = this;
+		var _paperActivityCheckerData = null;
+		self.activityConfigurations = new Object();
+		self.activities = [];
 
 
-        function add() {
-            var loggedUser = ContextService.getLoggedUser();
-
-            getSelectedParticipant()
-                .then(function (selectedParticipant) {
-                    _paperActivityCheckerData = JSON.parse(window.sessionStorage.getItem('activityPaper'));
-                    if (_paperActivityCheckerData) {
-                        _paperActivityCheckerData.realizationDate = new Date(_paperActivityCheckerData.realizationDate);
-                        ActivityRepositoryService.createFromPaperActivity(self.listSurveys, loggedUser, selectedParticipant, _paperActivityCheckerData, self.activityConfigurations);
-                        _paperActivityCheckerData = null;
-                        window.sessionStorage.removeItem('activityPaper');
-                        window.sessionStorage.removeItem('activityType');
-                    } else {
-                        ActivityRepositoryService.createFromSurvey(self.listSurveys, loggedUser, selectedParticipant, self.activityConfigurations);
-                    }
-                });
-        }
-
-        function createActivityDto(survey, configuration, mode){
-            let loggedUser = ContextService.getLoggedUser();
-            let activityDto = ActivityDtoFactory.create(survey, configuration, mode , loggedUser);
-            return activityDto;
-        }
-
-        function saveActivities(activityDtos){
-            _prepareActivities(activityDtos)
-                .then(() => ActivityRepositoryService.saveActivities(self.activities))
-                .then(() => ApplicationStateService.activateParticipantActivities())
-                .then(() => self.activities = []);
+		/* Public methods */
+		self.initializePaperActivityData = initializePaperActivityData;
+		self.add = add;
+		self.listAll = listAll;
+		self.listAllCategories = listAllCategories;
+		self.listAvailables = listAvailables;
+		self.selectActivities = selectActivities;
+		self.getSelectedActivities = getSelectedActivities;
+		self.getSelectedParticipant = getSelectedParticipant;
+		self.listActivityCheckers = listActivityCheckers;
+		self.getById = getById;
+		self.setActivitiesSelection = setActivitiesSelection;
+		self.getActivitiesSelection = getActivitiesSelection;
+		self.updateCheckerActivity = updateCheckerActivity;
+		self.configurationStructure = configurationStructure;
+		self.addActivityRevision = addActivityRevision;
+		self.getActivityRevisions = getActivityRevisions;
+		self.createActivityDto = createActivityDto;
+		self.saveActivities = saveActivities;
+		self.getSurveyFromJson = getSurveyFromJson;
 
 
-            window.sessionStorage.removeItem('activityDtos');
-        }
+		function add() {
+			var loggedUser = ContextService.getLoggedUser();
 
-        function _prepareActivities(activityDtos){
-            return getSelectedParticipant().then(function (selectedParticipant) {
-                activityDtos.forEach(activityDto => {
-                    activityDto.mode === "ONLINE" ? _createOnLineActivity(activityDto, selectedParticipant):_createPaperActivity(activityDto, selectedParticipant);
-                });
-            });
-        }
+			getSelectedParticipant()
+				.then(function (selectedParticipant) {
+					_paperActivityCheckerData = JSON.parse(window.sessionStorage.getItem('activityPaper'));
+					if (_paperActivityCheckerData) {
+						_paperActivityCheckerData.realizationDate = new Date(_paperActivityCheckerData.realizationDate);
+						ActivityRepositoryService.createFromPaperActivity(self.listSurveys, loggedUser, selectedParticipant, _paperActivityCheckerData, self.activityConfigurations);
+						_paperActivityCheckerData = null;
+						window.sessionStorage.removeItem('activityPaper');
+						window.sessionStorage.removeItem('activityType');
+					} else {
+						ActivityRepositoryService.createFromSurvey(self.listSurveys, loggedUser, selectedParticipant, self.activityConfigurations);
+					}
+				});
+		}
 
-        function _createOnLineActivity(activityDto, selectedParticipant){
-            ActivityRepositoryService.createOnLineActivity(activityDto.surveyForm, activityDto.user, selectedParticipant, activityDto.configuration)
-                .then(OnlineActivity => self.activities.push(OnlineActivity));
+		function createActivityDto(survey, configuration, mode) {
+			let loggedUser = ContextService.getLoggedUser();
+			let activityDto = ActivityDtoFactory.create(survey, configuration, mode, loggedUser);
+			return activityDto;
+		}
 
-        }
-
-        function _createPaperActivity(activityDto, selectedParticipant){
-            ActivityRepositoryService.createPaperActivity(activityDto.surveyForm,
-                activityDto.user, selectedParticipant, activityDto.paperActivityData, activityDto.configuration)
-                .then(paperActivity => self.activities.push(paperActivity));
-        }
+		function saveActivities(activityDtos) {
+			_prepareActivities(activityDtos)
+				.then(() => ActivityRepositoryService.saveActivities(self.activities))
+				.then(() => ApplicationStateService.activateParticipantActivities())
+				.then(() => self.activities = []);
 
 
-        function setActivitiesSelection(surveys) {
-            self.listSurveys = surveys;
-            _constructCollectionConfiguration();
-        }
+			window.sessionStorage.removeItem('activityDtos');
+		}
 
-        function getActivitiesSelection() {
-            return self.listSurveys;
-        }
+		function _prepareActivities(activityDtos) {
+			return getSelectedParticipant().then(function (selectedParticipant) {
+				activityDtos.forEach(activityDto => {
+					activityDto.mode === "ONLINE" ? _createOnLineActivity(activityDto, selectedParticipant) : _createPaperActivity(activityDto, selectedParticipant);
+				});
+			});
+		}
 
-        function listAll() {
-            return getSelectedParticipant()
-                .then(function (selectedParticipant) {
-                    return ActivityRepositoryService.listAll(selectedParticipant);
-                });
-        }
+		function _createOnLineActivity(activityDto, selectedParticipant) {
+			ActivityRepositoryService.createOnLineActivity(activityDto.surveyForm, activityDto.user, selectedParticipant, activityDto.configuration)
+				.then(OnlineActivity => self.activities.push(OnlineActivity));
 
-        function listAvailables() {
-            return ActivityRepositoryService.listAvailables();
-        }
+		}
 
-        function getById(activityInfo) {
-            return ActivityRepositoryService.getById(activityInfo);
-        }
+		function _createPaperActivity(activityDto, selectedParticipant) {
+			ActivityRepositoryService.createPaperActivity(activityDto.surveyForm,
+				activityDto.user, selectedParticipant, activityDto.paperActivityData, activityDto.configuration)
+				.then(paperActivity => self.activities.push(paperActivity));
+		}
 
-        function initializePaperActivityData(paperActivityCheckerData) {
-            window.sessionStorage.setItem('activityPaper', JSON.stringify(paperActivityCheckerData));
-            _paperActivityCheckerData = paperActivityCheckerData;
-        }
 
-        function selectActivities(activities) {
-            ContextService.selectActivities(activities);
-        }
+		function setActivitiesSelection(surveys) {
+			self.listSurveys = surveys;
+			_constructCollectionConfiguration();
+		}
 
-        function getSelectedActivities() {
-            return {
-                list: function list() {
-                    return ContextService.getSelectedActivities();
-                },
-                discard: function discard() {
-                    var toDiscard = ContextService.getSelectedActivities().map(function (activity) {
-                        activity.isDiscarded = true;
-                        return activity;
-                    });
-                    ActivityRepositoryService.discard(toDiscard);
-                    ContextService.clearSelectedActivities();
-                }
-            };
-        }
+		function getActivitiesSelection() {
+			return self.listSurveys;
+		}
 
-        function getSelectedParticipant() {
-            return ContextService.getSelectedParticipant();
-        }
+		function listAll() {
+			return getSelectedParticipant()
+				.then(function (selectedParticipant) {
+					return ActivityRepositoryService.listAll(selectedParticipant);
+				});
+		}
 
-        function listActivityCheckers() {
-            return UserRepositoryService.listAll();
-        }
+		function listAvailables() {
+			return ActivityRepositoryService.listAvailables();
+		}
 
-        function useSelectedActivity() {
-            var selectedActivities = getSelectedActivities();
-            if (selectedActivities.length === 1) {
-                ContextService.setActivityInUse(selectedActivities[0]);
-                ModuleService.ActivityFacadeService.useActivity(selectedActivities[0]);
-            }
-        }
+		function getById(activityInfo) {
+			return ActivityRepositoryService.getById(activityInfo);
+		}
 
-        /* Activity Configuration Methods */
-        function configurationStructure() {
-            return self.activityConfigurations;
-        }
+		function initializePaperActivityData(paperActivityCheckerData) {
+			window.sessionStorage.setItem('activityPaper', JSON.stringify(paperActivityCheckerData));
+			_paperActivityCheckerData = paperActivityCheckerData;
+		}
 
-        function _constructCollectionConfiguration() {
-            self.listSurveys.forEach(function (template) {
-                self.activityConfigurations[template.surveyTemplate.identity.acronym] = {};
-            });
-        }
+		function selectActivities(activities) {
+			ContextService.selectActivities(activities);
+		}
 
-        function listAllCategories() {
-            return ActivityRepositoryService.listAllCategories();
-        }
+		function getSelectedActivities() {
+			return {
+				list: function list() {
+					return ContextService.getSelectedActivities();
+				},
+				discard: function discard() {
+					var toDiscard = ContextService.getSelectedActivities().map(function (activity) {
+						activity.isDiscarded = true;
+						return activity;
+					});
+					ActivityRepositoryService.discard(toDiscard);
+					ContextService.clearSelectedActivities();
+				}
+			};
+		}
 
-        function updateCheckerActivity(recruitmentNumber, id, activityStatus) {
-            return ActivityRepositoryService.updateCheckerActivity(recruitmentNumber, Object.freeze({
-                id,
-                activityStatus
-            }));
-        }
+		function getSelectedParticipant() {
+			return ContextService.getSelectedParticipant();
+		}
 
-        function addActivityRevision(activityRevision, activity) {
-            return ActivityRepositoryService.addActivityRevision(activityRevision, activity);
-        }
+		function listActivityCheckers() {
+			return UserRepositoryService.listAll();
+		}
 
-        function getActivityRevisions(activityID, activity) {
-            return ActivityRepositoryService.getActivityRevisions(activityID, activity);
-        }
-    }
+		function useSelectedActivity() {
+			var selectedActivities = getSelectedActivities();
+			if (selectedActivities.length === 1) {
+				ContextService.setActivityInUse(selectedActivities[0]);
+				ModuleService.ActivityFacadeService.useActivity(selectedActivities[0]);
+			}
+		}
+
+		/* Activity Configuration Methods */
+		function configurationStructure() {
+			return self.activityConfigurations;
+		}
+
+		function _constructCollectionConfiguration() {
+			self.listSurveys.forEach(function (template) {
+				self.activityConfigurations[template.surveyTemplate.identity.acronym] = {};
+			});
+		}
+
+		function listAllCategories() {
+			return ActivityRepositoryService.listAllCategories();
+		}
+
+		function updateCheckerActivity(recruitmentNumber, id, activityStatus) {
+			return ActivityRepositoryService.updateCheckerActivity(recruitmentNumber, Object.freeze({
+				id,
+				activityStatus
+			}));
+		}
+
+		function addActivityRevision(activityRevision, activity) {
+			return ActivityRepositoryService.addActivityRevision(activityRevision, activity);
+		}
+
+		function getActivityRevisions(activityID, activity) {
+			return ActivityRepositoryService.getActivityRevisions(activityID, activity);
+		}
+
+		function getSurveyFromJson(json) {
+			return SurveyFactory.fromJsonObject(json);
+		}
+	}
 }());
