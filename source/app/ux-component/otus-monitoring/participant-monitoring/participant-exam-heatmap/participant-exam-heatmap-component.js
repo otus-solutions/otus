33ec26fefbdf195d561879b9c91a37e05998f439
LeoGut <leogut@gmail.com>
2020-01-02 14:16:08 -0300

    hotfix-OTUS-633 limpando código

--- source/app/ux-component/otus-monitoring/participant-monitoring/participant-exam-heatmap/participant-exam-heatmap-component.js
+++ source/app/ux-component/otus-monitoring/participant-monitoring/participant-exam-heatmap/participant-exam-heatmap-component.js
@@ -5,9 +5,9 @@
     .module('otusjs.otus.uxComponent')
     .component('otusParticipantExamHeatmap', {
       controller: "otusParticipantExamHeatmapCtrl as $ctrl",
-      templateUrl: 'app/ux-component/otus-monitoring/participant-monitoring/participant-exam-heatmap/participant-exam-heatmap-template.html',
-      bindings: {
-        selectedParticipant: '='
-      }
+      templateUrl: 'app/ux-component/otus-monitoring/participant-monitoring/participant-exam-heatmap/participant-exam-heatmap-template.html'//,
+      // bindings: {
+      //   selectedParticipant: '='
+      // }
     });
 }());
