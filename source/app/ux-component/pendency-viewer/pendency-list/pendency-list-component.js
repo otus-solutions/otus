ef77cef43570f565fae4f9a12701033460075c4a
Fabiano Ramires <fdrtec@gmail.com>
2020-02-17 20:32:39 -0300

    OTUS-645 prototipo inicial da tela de pendencia com icones representativos

--- source/app/ux-component/pendency-viewer/pendency-list/pendency-list-component.js
+++ source/app/ux-component/pendency-viewer/pendency-list/pendency-list-component.js
@@ -17,33 +17,33 @@
     //TODO: (OTUS-645) service for get List of Pendencies
     self.pendencies = [
       {
-        creationDate: "01/03/2020",
-        dueDate: "17/02/2020",
+        creationDate: "17/02/2020",
+        dueDate: "01/03/2020",
         requester: "supervisor@otus.com.br",
         receiver: "revisor@otus.com.br",
         acronym: "ELSA1",
         rn: 123567890,
-        status : "FINALIZED"
+        status : "FINALIZADO"
       },
 
       {
-        creationDate: "02/03/2020",
-        dueDate: "17/02/2020",
+        creationDate: "17/02/2020",
+        dueDate: "02/03/2020",
         requester: "supervisor@otus.com.br",
         receiver: "revisor@otus.com.br",
         acronym: "ELSA2",
         rn: 9874563210,
-        status : "FINALIZED"
+        status : "FINALIZADO"
       },
 
       {
-        creationDate: "03/03/2020",
-        dueDate: "17/02/2020",
+        creationDate: "17/02/2020",
+        dueDate: "03/03/2020",
         requester: "supervisor@otus.com.br",
         receiver: "revisor@otus.com.br",
         acronym: "ELSA3",
         rn: 4569871230,
-        status : "FINALIZED"
+        status : "FINALIZADO"
       },
 
 
