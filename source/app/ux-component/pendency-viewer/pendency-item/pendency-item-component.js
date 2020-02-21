00456e0506cdb2efc1d6bde2af1f104dad701cda
Fabiano Ramires <fdrtec@gmail.com>
2020-02-21 09:50:55 -0300

    OTUS-645 ajustando visualizaçãp de datas e tempo limite

--- source/app/ux-component/pendency-viewer/pendency-item/pendency-item-component.js
+++ source/app/ux-component/pendency-viewer/pendency-item/pendency-item-component.js
@@ -16,6 +16,26 @@
   function Controller() {
     const self = this;
 
+    self.dueDate = _formatDate(new Date(self.item.dueDate));
+    self.creationDate = _formatDate(new Date(self.item.creationDate));
+    self.remainingDays = calculateRemainingDays(self.item.dueDate);
+
+    function _formatDate(date) {
+      return date.getDate() + "/"+ (date.getMonth()+1) + "/" + date.getFullYear();
+    }
+
+    function calculateRemainingDays(dueDate){
+      let today = _extractDateZeroTime(new Date());
+      let due = _extractDateZeroTime(new Date(dueDate));
+      const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
+      let deadLine = Math.floor((due - today) / MILLISECONDS_PER_DAY);
+      return deadLine;
+    }
+
+    function _extractDateZeroTime(date){
+      date.setHours(0,0,0,0);
+      return date;
+    }
   }
 
 }());
