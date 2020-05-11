05750db49924e4980e4eeb2cfcecd95ca591358d
leogut <leogut@gmail.com>
2020-05-11 13:10:43 -0300

    feature-OTUS-705 Mensagens (Toast) ao clicar no botão de recuperação de senha.

--- source/app/ux-component/participant-password-reset-button/participant-password-reset-button-component.js
+++ source/app/ux-component/participant-password-reset-button/participant-password-reset-button-component.js
@@ -10,16 +10,20 @@
     });
 
   Controller.$inject = [
-    'otusjs.participant.business.ParticipantManagerService'
+    'otusjs.participant.business.ParticipantManagerService',
+    'otusjs.participant.business.ParticipantMessagesService'
   ];
 
-  function Controller(ParticipantManagerService) {
+  function Controller(ParticipantManagerService, ParticipantMessagesService) {
     var self = this;
 
     self.resetPassword = resetPassword;
 
     function resetPassword() {
-      ParticipantManagerService.requestPasswordReset(self.email);
+      ParticipantMessagesService.showToast('Enviando e-mail de recuperação de senha...');
+      ParticipantManagerService.requestPasswordReset(self.email)
+        .then(() => ParticipantMessagesService.showToast('E-mail enviado!'))
+        .catch(() => ParticipantMessagesService.showToast('Falha ao enviar e-mail.'));
     }
 
   }
