7ed106db847f6f48ec7e36e86bb23965b21df721
FlaviaAP <flavia.avila@ufrgs.br>
2020-05-22 16:02:34 -0300

    OUTS-730 geração do template dos filtros em generic-list-control via bind do nome do componente dos filtros

--- source/app/ux-component/generic-list-viewer/generic-list-control/generic-list-control-component.js
+++ source/app/ux-component/generic-list-viewer/generic-list-control/generic-list-control-component.js
@@ -7,26 +7,37 @@
       controller: 'genericListControlCtrl as $ctrl',
       templateUrl: 'app/ux-component/generic-list-viewer/generic-list-control/generic-list-control-template.html',
       bindings: {
+        filtersComponentName: '<',
         getItems: '&',
         searchSettings: '=',
         itemAttributes: '<',
         paginatorActive: '='
-
       }
     }).controller('genericListControlCtrl', Controller);
 
   Controller.$inject = [
-    'GENERIC_LIST_VIEWER_LABELS'
+    'GENERIC_LIST_VIEWER_LABELS',
+    'otusjs.utils.HtmlBuilderService'
   ];
 
-  function Controller(GENERIC_LIST_VIEWER_LABELS) {
+  function Controller(GENERIC_LIST_VIEWER_LABELS, HtmlBuilderService) {
     const self = this;
     self.GENERIC_LIST_VIEWER_LABELS = GENERIC_LIST_VIEWER_LABELS;
+    self.filtersViewEnable = false;
 
+    self.$onInit = onInit;
     self.openFilters = openFilters;
     self.closeFilters = closeFilters;
 
-    self.filtersViewEnable = false;
+
+    function onInit(){
+      const tag = HtmlBuilderService.generateTagName(self.filtersComponentName);
+      self.filtersTemplate = '<' + tag + ' ng-hide="!$ctrl.filtersViewEnable"' +
+        '                             class="filter"' +
+        '                             search-settings="$ctrl.searchSettings"' +
+        '                             item-attributes="$ctrl.itemAttributes"' +
+        '                             paginator-active="$ctrl.paginatorActive"/>';
+    }
 
     function openFilters() {
       self.filtersViewEnable = true;
