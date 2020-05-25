7b99ffdc95568df755235d4297afa978844a129d
FlaviaAP <flavia.avila@ufrgs.br>
2020-05-25 10:26:14 -0300

    OTUS-730 refatoração de pendency-viewer-component como 'filho' de generic-list-viewr-component

--- source/app/ux-component/pendency-viewer/pendency-viewer-component.js
+++ source/app/ux-component/pendency-viewer/pendency-viewer-component.js
@@ -9,44 +9,16 @@
       bindings: {}
     }).controller('pendencyViewCtrl', Controller);
 
-  Controller.$inject = ['otusjs.pendencyViewer.PendencyViewerService', 'PENDENCY_VIEWER_TITLES'];
+  Controller.$inject = ['otusjs.pendencyViewer.PendencyViewerService'];
 
-  function Controller(PendencyViewerService, PENDENCY_VIEWER_TITLES) {
+  function Controller(PendencyViewerService) {
     const self = this;
-
     self.items = [];
     self.paginatorActive = false;
-    self.PENDENCY_VIEWER_TITLES = PENDENCY_VIEWER_TITLES;
-    self.callValidationItemsLimits = PendencyViewerService.callValidationItemsLimits;
+    self.viewerTitle = PendencyViewerService.LABELS.PAGE_TITLE;
+    self.viewerService = PendencyViewerService;
     self.itemComponentName = 'otusPendencyItem';
     self.filtersComponentName = 'otusPendecyListFilters';
-
-    self.$onInit = onInit;
-    self.getAllItems = getAllItems;
-
-    function onInit() {
-      self.searchSettings = PendencyViewerService.getSearchSettings();
-      self.itemAttributes = PendencyViewerService.getPendencyAttributes();
-      getAllItems(self.searchSettings);
-    }
-
-    function getAllItems(searchSettings) {
-      PendencyViewerService.getAllItems(searchSettings)
-        .then(data => {
-          self.items = data;
-          self.paginatorActive = !!self.items.length;
-          if(self.paginatorActive) {
-            _prepareParametersForPagination(searchSettings);
-          }
-        });
-    }
-
-    function _prepareParametersForPagination(searchSettings) {
-      if (self.stuntmanSearchSettings) {
-        self.stuntmanSearchSettings = null;
-      }
-      self.stuntmanSearchSettings = angular.copy(searchSettings);
-    }
   }
 
 }());
