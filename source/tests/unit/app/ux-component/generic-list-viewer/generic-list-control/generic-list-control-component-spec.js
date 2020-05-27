a43db0b89f22ec4154db2bdce6c8759bca668751
FlaviaAP <flavia.avila@ufrgs.br>
2020-05-27 11:09:59 -0300

    OTUS-730 testes unitários de generic-list-control-component

--- source/tests/unit/app/ux-component/generic-list-viewer/generic-list-control/generic-list-control-component-spec.js
+++ source/tests/unit/app/ux-component/generic-list-viewer/generic-list-control/generic-list-control-component-spec.js
@@ -1,13 +1,19 @@
-describe('otusPendencyListControlComponent_UnitTest_Suite', () => {
+describe('otusGenericListControlComponent_UnitTest_Suite', () => {
   let ctrl;
   let Injections = [];
+
+  const FILTERS_COMPONENT_NAME = 'filtersComponentName';
+  const FILTERS_TEMPLATE_TAG = 'filters-component-name';
 
   beforeEach(() => {
     angular.mock.module('otusjs.otus');
     angular.mock.inject(($injector,$controller) => {
 
-      Injections.PENDENCY_VIEWER_TITLES = $injector.get('PENDENCY_VIEWER_TITLES');
-      ctrl = $controller('pendencyListControlCtrl', Injections);
+      Injections.GENERIC_LIST_VIEWER_LABELS = $injector.get('GENERIC_LIST_VIEWER_LABELS');
+      Injections.HtmlBuilderService = $injector.get('otusjs.utils.HtmlBuilderService');
+      ctrl = $controller('genericListControlCtrl', Injections, {
+        filtersComponentName: FILTERS_COMPONENT_NAME
+      });
 
     });
   });
@@ -19,6 +25,12 @@ describe('otusPendencyListControlComponent_UnitTest_Suite', () => {
   it('ctrlMethodsExistence_check', () => {
     expect(ctrl.openFilters).toBeDefined();
     expect(ctrl.closeFilters).toBeDefined();
+  });
+
+  it('onInitMethod_should_set_filtersTemplate', () => {
+    ctrl.$onInit();
+    expect(ctrl.filtersTemplate).toBeDefined();
+    expect(ctrl.filtersTemplate.includes('<'+FILTERS_TEMPLATE_TAG)).toBeTruthy();
   });
 
   it('openFiltersMethod_should_attribute', () => {
