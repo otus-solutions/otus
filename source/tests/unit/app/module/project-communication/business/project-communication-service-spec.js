761ce32dfb603b6a4a650ec2994210b7181a733f
adonisgarcia <adonis.garcia.adg@gmail.com>
2020-05-28 10:59:39 -0300

    OTUS-730 teste de unidade module e context e business

---
+++ source/tests/unit/app/module/project-communication/business/project-communication-service-spec.js
@@ -0,0 +1,72 @@
+describe('ProjectCommunicationService_UnitTest_Suite', () => {
+  let service;
+  let Injections = [];
+  let Mock = {};
+  let LIMIT = "12";
+
+  beforeEach(() => {
+    angular.mock.module('otusjs.otus');
+    angular.mock.inject($injector => {
+      Injections.ProjectCommunicationRepositoryService = $injector.get('otusjs.project.communication.repository.ProjectCommunicationRepositoryService');
+      service = $injector.get('otusjs.project.communication.business.ProjectCommunicationService', Injections);
+      spyOn(Injections.ProjectCommunicationRepositoryService, 'createMessage');
+      spyOn(Injections.ProjectCommunicationRepositoryService, 'createIssue');
+      spyOn(Injections.ProjectCommunicationRepositoryService, 'getProjectCommunicationById');
+      spyOn(Injections.ProjectCommunicationRepositoryService, 'getProjectCommunicationByIdLimit');
+      spyOn(Injections.ProjectCommunicationRepositoryService, 'updateReopen');
+      spyOn(Injections.ProjectCommunicationRepositoryService, 'updateClose');
+      spyOn(Injections.ProjectCommunicationRepositoryService, 'listIssue');
+
+    });
+  });
+
+  it('serviceExistence_check', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('serviceMethodsExistence_check', () => {
+    expect(service.createMessage).toBeDefined();
+    expect(service.createIssue).toBeDefined();
+    expect(service.getProjectCommunicationById).toBeDefined();
+    expect(service.getProjectCommunicationByIdLimit).toBeDefined();
+    expect(service.updateReopen).toBeDefined();
+    expect(service.updateClose).toBeDefined();
+    expect(service.listIssue).toBeDefined();
+  });
+
+  it('createMessageMethod_should_evoke_createMessage_by_repositoryService', () => {
+    service.createMessage(Mock._id, Mock);
+    expect(Injections.ProjectCommunicationRepositoryService.createMessage).toHaveBeenCalledTimes(1)
+  });
+
+  it('createIssueMethod_should_evoke_createIssue_by_repositoryService', () => {
+    service.createIssue(Mock);
+    expect(Injections.ProjectCommunicationRepositoryService.createIssue).toHaveBeenCalledTimes(1)
+  });
+
+  it('getProjectCommunicationByIdMethod_should_evoke_getProjectCommunicationById_by_repositoryService', () => {
+    service.getProjectCommunicationById(Mock._id);
+    expect(Injections.ProjectCommunicationRepositoryService.getProjectCommunicationById).toHaveBeenCalledTimes(1)
+  });
+
+  it('getProjectCommunicationByIdLimitMethod_should_evoke_getProjectCommunicationByIdLimit_by_repositoryService', () => {
+    service.getProjectCommunicationByIdLimit(Mock._id, LIMIT);
+    expect(Injections.ProjectCommunicationRepositoryService.getProjectCommunicationByIdLimit).toHaveBeenCalledTimes(1)
+  });
+
+  it('updateReopenMethod_should_evoke_updateReope_by_repositoryService', () => {
+    service.updateReopen(Mock._id);
+    expect(Injections.ProjectCommunicationRepositoryService.updateReopen).toHaveBeenCalledTimes(1)
+  });
+
+  it('updateCloseMethod_should_evoke_updateClose_by_repositoryService', () => {
+    service.updateClose(Mock._id);
+    expect(Injections.ProjectCommunicationRepositoryService.updateClose).toHaveBeenCalledTimes(1)
+  });
+
+  it('listIssueMethod_should_evoke_listIssue_by_repositoryService', () => {
+    service.listIssue();
+    expect(Injections.ProjectCommunicationRepositoryService.listIssue).toHaveBeenCalledTimes(1)
+  });
+
+});
