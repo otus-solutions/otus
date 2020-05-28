ba90c79ce6e2a1fa24a333ec43bd8d962e83264d
FlaviaAP <flavia.avila@ufrgs.br>
2020-05-28 19:47:06 -0300

    OTUS-730 refatoração das constantes ISSUE_VIEWER_LABELS e dos métodos 'fake' de IssueViewerService

--- source/app/ux-component/project-communication/issue-viewer/issue-viewer-constant.js
+++ source/app/ux-component/project-communication/issue-viewer/issue-viewer-constant.js
@@ -6,9 +6,55 @@
     .constant('ISSUE_VIEWER_LABELS', {
       PAGE_TITLE:'Visualizador de Issues',
 
+      ISSUE_ATTRIBUTES: {
+        RN: {
+          TITLE: 'rn',
+          TRANSLATED_TITLE: 'Recrutamento',
+          ICON: 'account_circle'
+        },
+        SENDER_NAME: {
+          TITLE: 'name',
+          TRANSLATED_TITLE: 'Nome',
+          ICON: 'person'
+        },
+        CENTER: {
+          TITLE: 'center',
+          TRANSLATED_TITLE: 'Centro',
+          ICON: 'location_on'
+        },
+        CREATION_DATE: {
+          TITLE: 'creationDate',
+          TRANSLATED_TITLE: 'Criação',
+          ICON: 'schedule'
+        },
+        TITLE: {
+          TITLE: 'title',
+          TRANSLATED_TITLE: 'Título',
+          ICON: 'insert_comment'
+        },
+        STATUS: {
+          TITLE: 'status',
+          ICON: 'assignment_ind',
+          OPENED: {
+            TRANSLATED_TITLE: 'Abertos',
+            VALUE: 'OPENED'
+          },
+          CLOSED: {
+            TRANSLATED_TITLE: 'Fechados',
+            VALUE: 'CLOSED'
+          },
+          FINALIZED: {
+            TRANSLATED_TITLE: 'Finalizados',
+            VALUE: 'FINALIZED'
+          }
+        }
+      },
+
       FILTER: {
         RN: 'Recrutamento',
+        NAME: 'Nome',
         CREATION_DATE: 'Criação',
+        TITLE: 'Título',
         CENTER: 'Centro',
         STATUS: {
           OPENED:'Abertos',
--- source/app/ux-component/project-communication/issue-viewer/issue-viewer-service.js
+++ source/app/ux-component/project-communication/issue-viewer/issue-viewer-service.js
@@ -6,13 +6,14 @@
     .service('otusjs.issueViewer.IssueViewerService', Service);
 
   Service.$inject = [
+    '$q',
     'otusjs.genericListViewer.GenericListViewerService',
     'ISSUE_VIEWER_LABELS',
     // 'otusjs.pendency.repository.UserActivityPendencyRepositoryService',
     // 'otusjs.model.pendency.UserActivityPendencyFactory',
   ];
 
-  function Service(GenericListViewerService, ISSUE_VIEWER_LABELS,
+  function Service($q, GenericListViewerService, ISSUE_VIEWER_LABELS,
                    // UserActivityPendencyRepositoryService, UserActivityPendencyFactory
                    ) {
 
@@ -20,16 +21,21 @@
     const INITIAL_CURRENT_QUANTITY = 0;
     const INITIAL_QUANTITY_TO_GET = 15;
 
+    const fake_genericListFactory = {
+      fromJsonObject: function(item) { return item; }
+    };
+
     initialize();
 
     self.initialize = initialize;
     self.getChecker = getChecker;
-    self.findRecruitmentNumberFromEmail = findRecruitmentNumberFromEmail;
+    self.findParticipantFromEmail = findParticipantFromEmail;
+    self.translateStatus = translateStatus;
 
     function initialize(){
       angular.extend(self, self, GenericListViewerService);
-      self.init(ISSUE_VIEWER_LABELS, INITIAL_CURRENT_QUANTITY, INITIAL_QUANTITY_TO_GET
-        // UserActivityPendencyRepositoryService.getAllPendencies, UserActivityPendencyFactory
+      self.init(ISSUE_VIEWER_LABELS, INITIAL_CURRENT_QUANTITY, INITIAL_QUANTITY_TO_GET,
+        fake_getAllItems, fake_genericListFactory //todo temp
       );
 
       self.getSearchSettings = getSearchSettings;
@@ -37,26 +43,7 @@
       self.getInputViewState = getInputViewState;
       self.getSelectedParticipantRN = getSelectedParticipantRN;
 
-      self.getItemAttributes = getItemAttributesFAKE; //todo excluir
-    }
-
-    function getItemAttributesFAKE(){
-      function getFakeIssue(id, sender, creationDate) {
-        return {
-          "_id": id,
-          "objectType": "Issue",
-          "sender": sender,
-          // "title": "Não consigo preencher a atividade TCLEC",
-          // "message": "Quando tento responder uma pergunta, não consigo inserir a resposta",
-          "creationDate": creationDate.toJSON(),
-          "status": "OPEN"
-        }
-      }
-      return [
-        getFakeIssue('1111111', 'aaa@otus.com', new Date(2020,5, 5)),
-        getFakeIssue('2222222', 'ccc@otus.com', new Date(2020,4, 5)),
-        getFakeIssue('3333333', 'bbb@otus.com', new Date(2020,4, 25)),
-      ]
+      self.getAllItemsFromRepositoryService = fake_getAllItems;//todo temp
     }
 
     function getSearchSettings() {
@@ -68,17 +55,25 @@
           "mode": 1
         },
         "filter": {
-          "status": "OPENED"
+          "status": ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.STATUS.OPENED.VALUE
         }
       };
     }
 
     function getItemAttributes() {
       return {
-        rn: {title: 'rn', translatedTitle: ISSUE_VIEWER_LABELS.RN, icon: 'account_circle'},
-        center: {title: 'center', translatedTitle: ISSUE_VIEWER_LABELS.CENTER, icon: 'assignment'},
-        creationDate: {title: 'creationDate', translatedTitle: ISSUE_VIEWER_LABELS.CREATION_DATE, icon: 'schedule'},
-        status: {title: 'status', translatedTitle: ISSUE_VIEWER_LABELS.RECEIVER, icon: 'assignment_ind'}
+        rn:           ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.RN,
+        name:         ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.SENDER_NAME,
+        center:       ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.CENTER,
+        creationDate: ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.CREATION_DATE,
+        title:        ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.TITLE,
+        status:       ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.STATUS
+        // rn:           {title: 'rn',           translatedTitle: ISSUE_VIEWER_LABELS.FILTER.RN,             icon: 'account_circle'},
+        // name:         {title: 'name',         translatedTitle: ISSUE_VIEWER_LABELS.FILTER.NAME,           icon: 'person'},
+        // center:       {title: 'center',       translatedTitle: ISSUE_VIEWER_LABELS.FILTER.CENTER,         icon: 'location_on'},
+        // creationDate: {title: 'creationDate', translatedTitle: ISSUE_VIEWER_LABELS.FILTER.CREATION_DATE,  icon: 'schedule'},
+        // title:        {title: 'title',        translatedTitle: ISSUE_VIEWER_LABELS.FILTER.TITLE,          icon: 'insert_comment'},
+        // status:       {title: 'status',       translatedTitle: ISSUE_VIEWER_LABELS.FILTER.STATUS,         icon: 'assignment_ind'}
       };
     }
 
@@ -98,8 +93,43 @@
       searchSettings.filter[pendencyFilterItem.title] = [user.checker.email];
     }
 
-    function findRecruitmentNumberFromEmail(email){
-      return 1234567; //todo
+    function translateStatus(status){
+      const translation = ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.STATUS[status].TRANSLATED_TITLE;
+      return translation.substring(0, translation.length-1);
+    }
+
+    //todo
+    function findParticipantFromEmail(email){
+      return {
+        rn: 1234567,
+        name: 'Fulano de Tal',
+        center: 'RS'
+      };
+    }
+
+    //todo temp
+    function fake_getAllItems(searchSettings){
+
+      function getFakeIssue(id, sender, creationDate, status) {
+        return {
+          "_id": id,
+          "objectType": "Issue",
+          "sender": sender,
+          "title": "Não consigo preencher a atividade TCLEC " + id,
+          // "message": "Quando tento responder uma pergunta, não consigo inserir a resposta",
+          "creationDate": creationDate.toJSON(),
+          "status": status
+        }
+      }
+
+      const defer = $q.defer();
+      defer.resolve([
+        getFakeIssue('1111111', 'aaa@otus.com', new Date(2020,5, 5), "CLOSED"),
+        getFakeIssue('2222222', 'ccc@otus.com', new Date(2020,4, 5), "OPENED"),
+        getFakeIssue('3333333', 'bbb@otus.com', new Date(2020,4, 25), "FINALIZED"),
+      ]);
+
+      return defer.promise;
     }
 
   }
