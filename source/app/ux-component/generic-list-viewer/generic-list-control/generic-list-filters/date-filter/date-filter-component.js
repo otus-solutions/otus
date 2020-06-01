ebe31c57f526bc6890e79b963a2a4af1815e1122
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-01 19:18:44 -0300

    OTUS-730 add user-filter-component em index.html

---
+++ source/app/ux-component/generic-list-viewer/generic-list-control/generic-list-filters/date-filter/date-filter-component.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('otusjs.otus.uxComponent')
+    .component('otusDateFilter', {
+      controller: 'dateFilterCtrl as $ctrl',
+      templateUrl: 'app/ux-component/generic-list-viewer/generic-list-control/generic-list-filters/date-filter/date-filter-template.html',
+      bindings: {
+        filterItem: '<',
+        viewerServiceGetChecker: '=',
+        searchSettings: '=',
+        clear: '=',
+        changePaginationViewState: '='
+      }
+    }).controller('dateFilterCtrl', Controller);
+
+  Controller.$inject = ['GENERIC_LIST_VIEWER_LABELS'];
+
+  function Controller(GENERIC_LIST_VIEWER_LABELS) {
+    const self = this;
+    self.CANCEL_BUTTON_ICON = GENERIC_LIST_VIEWER_LABELS.CONTROL_PANEL.CANCEL_BUTTON;
+  }
+
+}());
