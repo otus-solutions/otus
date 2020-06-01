b475b6ee0388a1443a427b6c2f94a9f91426fa25
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-01 19:11:23 -0300

    OTUS-730 UserFilter (component + template)

---
+++ source/app/ux-component/generic-list-viewer/generic-list-control/generic-list-filters/user-filter/user-filter-component.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('otusjs.otus.uxComponent')
+    .component('otusUserFilter', {
+      controller: 'userFilterCtrl as $ctrl',
+      templateUrl: 'app/ux-component/generic-list-viewer/generic-list-control/generic-list-filters/user-filter/user-filter-template.html',
+      bindings: {
+        filterItem: '<',
+        viewerServiceGetChecker: '=',
+        searchSettings: '=',
+        clear: '=',
+        changePaginationViewState: '='
+      }
+    }).controller('userFilterCtrl', Controller);
+
+  Controller.$inject = ['GENERIC_LIST_VIEWER_LABELS'];
+
+  function Controller(GENERIC_LIST_VIEWER_LABELS) {
+    const self = this;
+    self.CANCEL_BUTTON_ICON = GENERIC_LIST_VIEWER_LABELS.CONTROL_PANEL.CANCEL_BUTTON;
+  }
+
+}());
