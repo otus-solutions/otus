c479a587a0fde917a0d51baf668712136214225c
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-02 17:13:31 -0300

    OTUS-730 excluída chamada para buscar participante por email em issue-item

--- source/app/ux-component/project-communication/issue-viewer/issue-item/issue-item-component.js
+++ source/app/ux-component/project-communication/issue-viewer/issue-item/issue-item-component.js
@@ -12,17 +12,20 @@
       }
     }).controller('issueItemCtrl', Controller);
 
-  Controller.$inject = ['otusjs.issueViewer.IssueViewerService'];
+  Controller.$inject = [ 'otusjs.issueViewer.IssueViewerService' ];
 
   function Controller(IssueViewerService) {
     const self = this;
-    const participantData = IssueViewerService.findParticipantFromEmail(self.item.sender);
-    self.rn = participantData.rn;
-    self.name = participantData.name;
-    self.center = participantData.center;
-    self.creationDate = IssueViewerService.formatDate(new Date(self.item.creationDate));
-    self.status = IssueViewerService.translateStatus(self.item.status);
-    self.statusClass = `issue-status status-${self.item.status.toLowerCase()}`;
+    self.$onInit = onInit;
+
+    function onInit(){
+      self.rn = self.item.rn;
+      self.name = self.item.name;
+      self.center = self.item.center;
+      self.creationDate = IssueViewerService.formatDate(new Date(self.item.creationDate));
+      self.status = IssueViewerService.translateStatus(self.item.status);
+      self.statusClass = `issue-status status-${self.item.status.toLowerCase()}`;
+    }
   }
 
 }());
