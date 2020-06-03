cb66c7455b6158bc85675f63b7e02e3c89c97cda
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-03 16:09:02 -0300

    OTUS-730 ajustes em issueViewerService

--- source/app/ux-component/project-communication/issue-viewer/issue-viewer-service.js
+++ source/app/ux-component/project-communication/issue-viewer/issue-viewer-service.js
@@ -16,27 +16,23 @@
 
   function Service($q, GenericListViewerService, ISSUE_VIEWER_LABELS,
                    ProjectCommunicationRepositoryService, IssueFactory,
-                   ParticipantManagerService
-                   ) {
+                   ParticipantManagerService) {
 
     const self = this;
     const INITIAL_CURRENT_QUANTITY = 0;
     const INITIAL_QUANTITY_TO_GET = 15;
 
     self.dataReady = false;
-    self.currParticipants = {};
+    self.participants = {};
 
     self.initialize = initialize;
-    self.getChecker = getChecker;
-    self.findParticipantFromEmail = findParticipantFromEmail;
     self.translateStatus = translateStatus;
-    self.setupParticipantList = setupParticipantList;
 
     initialize();
 
     function initialize(){
       angular.extend(self, self, GenericListViewerService);
-      self.init(self, ISSUE_VIEWER_LABELS, INITIAL_CURRENT_QUANTITY, INITIAL_QUANTITY_TO_GET,
+      self.init(ISSUE_VIEWER_LABELS, INITIAL_CURRENT_QUANTITY, INITIAL_QUANTITY_TO_GET,
         ProjectCommunicationRepositoryService.filter, IssueFactory,
         childParseItemsMethod
       );
@@ -44,33 +40,18 @@
       self.getSearchSettings = getSearchSettings;
       self.getItemAttributes = getItemAttributes;
       self.getInputViewState = getInputViewState;
-      self.getSelectedParticipantRN = getSelectedParticipantRN;
 
       ParticipantManagerService.setup()
         .then(response => { self.dataReady = true; });
     }
 
-    function setupParticipantList(){
-      return ParticipantManagerService.getParticipantList()
-    }
-
     function childParseItemsMethod(genericListJsonArray) {
-      console.log(genericListJsonArray)
-      let emails = genericListJsonArray.map(item => item.sender);
-
-      return ParticipantManagerService.setup()
-        .then(response => {
-          ParticipantManagerService.getParticipantList()
-            .filter(participant => emails.includes(participant.email))
-            .forEach(participant => self.currParticipants[participant.email] = participant);
-
-          let parsedItems = [];
-          genericListJsonArray.forEach(item => {
-            // parsedItems.push(IssueFactory.fromJsonObject(self.currParticipants[item.sender]));
-            parsedItems.push(IssueFactory.fromJsonObject(item));
-          });
-          return parsedItems;
-        });
+      let parsedItems = [];
+      genericListJsonArray.forEach(item => {
+        // parsedItems.push(IssueFactory.fromJsonObject(self.currParticipants[item.sender]));
+        parsedItems.push(IssueFactory.fromJsonObject(item, ParticipantManagerService));
+      });
+      return parsedItems;
     }
 
     function getSearchSettings() {
@@ -82,20 +63,21 @@
           "mode": 1
         },
         "filter": {
-          "status": ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.STATUS.OPENED.VALUE
+          "status": "OPENED"
         }
       };
     }
 
     function getItemAttributes() {
-      return {
-        rn:           ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.RN,
-        name:         ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.SENDER_NAME,
-        center:       ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.CENTER,
-        creationDate: ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.CREATION_DATE,
-        title:        ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.TITLE,
-        status:       ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.STATUS
-      };
+      let itemAttributes = {};
+      Object.values(ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES).forEach(attribute => {
+        itemAttributes[attribute.TITLE] = {
+          title:           attribute.TITLE,
+          translatedTitle: attribute.TRANSLATED_TITLE,
+          icon:            attribute.ICON
+        }
+      });
+      return itemAttributes;
     }
 
     function getInputViewState() {
@@ -106,27 +88,9 @@
       };
     }
 
-    function getSelectedParticipantRN(participant, pendencyFilterItem, searchSettings) {
-      searchSettings.filter[pendencyFilterItem.title] = participant.recruitmentNumber;
-    }
-
-    function getChecker(user, pendencyFilterItem, searchSettings) {
-      searchSettings.filter[pendencyFilterItem.title] = [user.checker.email];
-    }
-
     function translateStatus(status){
-      const translation = ISSUE_VIEWER_LABELS.ISSUE_ATTRIBUTES.STATUS[status].TRANSLATED_TITLE;
+      const translation = ISSUE_VIEWER_LABELS.FILTER_STATUS[status];
       return translation.substring(0, translation.length-1);
-    }
-
-    //todo
-    function findParticipantFromEmail(email){
-      //return self.currParticipants[email];
-      return {
-        rn: 1234567,
-        name: 'Fulano de Tal',
-        center: 'RS'
-      };
     }
 
   }
