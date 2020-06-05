1ce7fec02f6f30872544da6b4e9a7eca817326a2
erickpoleto <erick-poleto@hotmail.com>
2020-06-05 11:14:50 -0300

    OFE-63 funções para permissões de acesso criadas

--- source/app/deploy/data-source/user-data-source-service.js
+++ source/app/deploy/data-source/user-data-source-service.js
@@ -64,36 +64,7 @@
             .then(response => {
               // TODO: better check permissions format (model?)
               if ('data' in response) {
-                UserContextService.setUserPermissions([
-                  {
-                    "objectType": "activityPermission",
-                    "participantActivityAccess": false
-                  },
-                  {
-                    "objectType": "ParticipantPermission",
-                    "participantListAccess": false,
-                    "participantCreateAccess": false,
-                    "anonymousParticipantAccess": false
-                  },
-                  {
-                    "objectType": "MonitoringPermission",
-                    "centerActivitiesAccess": false,
-                    "activityFlagsAccess": false,
-                    "laboratoryFlagsAccess": false,
-                    "laboratoryControlAccess": false,
-                    "pendencyVisualizerAccess": false
-                  },
-                  {
-                    "objectType": "SurveyGroupPermission",
-                    "groups": []
-                  },
-                  {
-                    "objectType": "LaboratoryPermission",
-                    "sampleTransportationAccess": false,
-                    "examLotsAccess": false,
-                    "examSendingAccess": false,
-                    "unattachedLaboratoriesAccess": false
-                  }]);
+                UserContextService.setUserPermissions(response.data.permissions);
               }
             }).catch(e => {
               LogoutService.forceLogout("Erro ao carregar permissões de usuário", "Você será redirecionado à tela de login.");
