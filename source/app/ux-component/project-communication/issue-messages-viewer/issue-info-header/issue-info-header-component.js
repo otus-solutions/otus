a87f0aeae60de14b9f260778dde284d146df4218
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-09 18:20:30 -0300

    OTUS-730 template e compnente para cabeçalho da lista de mensagens, com info da issue correspondente

---
+++ source/app/ux-component/project-communication/issue-messages-viewer/issue-info-header/issue-info-header-component.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('otusjs.otus.uxComponent')
+    .component('otusIssueInfoHeader', {
+      controller: 'issueInfoHeaderCtrl as $ctrl',
+      templateUrl: 'app/ux-component/project-communication/issue-messages-viewer/issue-info-header/issue-info-header-template.html'
+    }).controller('issueInfoHeaderCtrl', Controller);
+
+  Controller.$inject = [
+    'otusjs.issueMessagesViewer.IssueMessagesViewerService'
+  ];
+
+  function Controller(IssueMessagesViewerService) {
+    const self = this;
+
+    self.$onInit = onInit;
+
+    function onInit(){
+      self.issue = IssueMessagesViewerService.getCurrIssueInfo();
+    }
+
+  }
+
+}());
