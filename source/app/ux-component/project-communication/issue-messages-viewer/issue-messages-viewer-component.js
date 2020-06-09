3cace4cc32f7a5053c100806d4d566d2204026e5
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-09 18:23:26 -0300

    OTUS-730 add chamadas ao IssueMessagesViewerService na  lista de mensagens para renderizar uma issue e suas mensagens

--- source/app/ux-component/project-communication/issue-messages-viewer/issue-messages-viewer-component.js
+++ source/app/ux-component/project-communication/issue-messages-viewer/issue-messages-viewer-component.js
@@ -5,24 +5,46 @@
     .module('otusjs.otus.uxComponent')
     .component('otusIssueMessagesViewerComponent', {
       controller: 'issueMessagesViewerCtrl as $ctrl',
-      templateUrl: 'app/ux-component/project-communication/issue-messages-viewer/issue-messages-viewer-template.html'
+      templateUrl: 'app/ux-component/project-communication/issue-messages-viewer/issue-messages-viewer-template.html',
+      bindings: {
+        issueId: '<'
+      }
     }).controller('issueMessagesViewerCtrl', Controller);
 
   Controller.$inject = [
-    'otusjs.issueViewer.IssueViewerService',
+    'ISSUE_MESSAGES_VIEWER_CONSTANTS',
+    'otusjs.issueMessagesViewer.IssueMessagesViewerService',
+    'otusjs.deploy.LoadingScreenService',
     'otusjs.application.dialog.DialogShowService'
   ];
 
-  function Controller(IssueViewerService, DialogService) {
+  function Controller(ISSUE_MESSAGES_VIEWER_CONSTANTS, IssueMessagesViewerService, LoadingScreenService, DialogService) {
     const self = this;
 
-    console.log('ddddddddddddd')
+    self.viewerTitle = ISSUE_MESSAGES_VIEWER_CONSTANTS.PAGE_TITLE;
+    self.itemComponentName = 'otusIssueMessageItem';
 
-    // self.paginatorActive = false;
-    // self.viewerTitle = IssueViewerService.LABELS.PAGE_TITLE;
-    // self.viewerService = IssueViewerService;
-    // self.itemComponentName = 'otusIssueItem';
-    // self.filtersComponentName = 'otusIssuesListFilters';
+    self.$onInit = onInit;
+
+    function onInit(){
+      self.items = [];
+      self.itemAttributes = {};//todo
+
+      LoadingScreenService.start();
+
+      self.issue = IssueMessagesViewerService.getCurrIssueInfo();
+      // console.log(self.issue);
+
+      IssueMessagesViewerService.getAllItems()
+        .then(response => {
+          self.items = response;
+          LoadingScreenService.finish();
+        })
+        .catch(error => {
+          console.log(error);
+          LoadingScreenService.finish();
+        });
+    }
 
   }
 
