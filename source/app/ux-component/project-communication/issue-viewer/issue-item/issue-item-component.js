61ccc861879af2218ef65f16e44da08f7081ebd1
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-15 15:53:50 -0300

    OTUS-730 exclusão do método privado _capitalizeName de iteem-item-controller e uso do método herdado de IssueViewerService

--- source/app/ux-component/project-communication/issue-viewer/issue-item/issue-item-component.js
+++ source/app/ux-component/project-communication/issue-viewer/issue-item/issue-item-component.js
@@ -38,17 +38,12 @@
     function onInit() {
       self.expanded = false;
       self.showingLastMessageIcon = visibilityIcon['false'];
-
       self.rn = self.item.participant.rn;
-      self.name = _capitalizeName(self.item.participant.name);
+      self.name = IssueViewerService.capitalizeName(self.item.participant.name);
       self.center = self.item.participant.center;
       self.creationDate = IssueViewerService.formatDate(new Date(self.item.creationDate));
       self.status = IssueViewerService.translateStatus(self.item.status);
       self.statusColor = IssueViewerService.LABELS.STATUS_COLOR[self.item.status];
-    }
-
-    function _capitalizeName(name) {
-      return name.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
     }
 
     function openIssueMessages(){
