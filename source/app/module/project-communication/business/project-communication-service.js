8fd80251a7c886a2952598e97ea3e24ea765b27e
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-17 18:20:07 -0300

    OTUS-730 add métodos updateFinalzied, getAllIssues e getIssueSenderInfo em business/project-communication-service

--- source/app/module/project-communication/business/project-communication-service.js
+++ source/app/module/project-communication/business/project-communication-service.js
@@ -19,7 +19,10 @@
     self.getProjectCommunicationByIdLimit = getProjectCommunicationByIdLimit;
     self.updateReopen = updateReopen;
     self.updateClose = updateClose;
+    self.updateFinalized = updateFinalized;
     self.filter = filter;
+    self.getAllIssueMessages = getAllIssueMessages;
+    self.getIssueSenderInfo = getIssueSenderInfo;
 
     function createMessage(ProjectCommunication) {
       return ProjectCommunicationRepositoryService.createMessage(ProjectCommunication)
@@ -45,8 +48,20 @@
       return ProjectCommunicationRepositoryService.updateClose(foundProjectCommunicationId);
     }
 
+    function updateFinalized(foundProjectCommunicationId){
+      return ProjectCommunicationRepositoryService.updateFinalized(foundProjectCommunicationId);
+    }
+
     function filter(searchSettings){
       return ProjectCommunicationRepositoryService.filter(searchSettings);
+    }
+
+    function getAllIssueMessages(searchSettings){
+      return ProjectCommunicationRepositoryService.getAllIssueMessages(searchSettings);
+    }
+
+    function getIssueSenderInfo(searchSettings){
+      return ProjectCommunicationRepositoryService.getIssueSenderInfo(searchSettings);
     }
 
   }
