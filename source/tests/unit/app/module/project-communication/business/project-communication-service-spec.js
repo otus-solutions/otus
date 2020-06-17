2af6296bc1996ec5af5e2a5dae58f7b88e073ef7
FlaviaAP <flavia.avila@ufrgs.br>
2020-06-17 18:26:56 -0300

    OTUS-730 refatoração dos testes unitários de project-communication-service

--- source/tests/unit/app/module/project-communication/business/project-communication-service-spec.js
+++ source/tests/unit/app/module/project-communication/business/project-communication-service-spec.js
@@ -15,9 +15,10 @@ describe('ProjectCommunicationService_UnitTest_Suite', () => {
       spyOn(Injections.ProjectCommunicationRepositoryService, 'getProjectCommunicationByIdLimit');
       spyOn(Injections.ProjectCommunicationRepositoryService, 'updateReopen');
       spyOn(Injections.ProjectCommunicationRepositoryService, 'updateClose');
-      spyOn(Injections.ProjectCommunicationRepositoryService, 'listIssue');
+      spyOn(Injections.ProjectCommunicationRepositoryService, 'updateFinalized');
       spyOn(Injections.ProjectCommunicationRepositoryService, 'filter');
-
+      spyOn(Injections.ProjectCommunicationRepositoryService, 'getAllIssueMessages');
+      spyOn(Injections.ProjectCommunicationRepositoryService, 'getIssueSenderInfo');
     });
   });
 
@@ -32,48 +33,58 @@ describe('ProjectCommunicationService_UnitTest_Suite', () => {
     expect(service.getProjectCommunicationByIdLimit).toBeDefined();
     expect(service.updateReopen).toBeDefined();
     expect(service.updateClose).toBeDefined();
-    expect(service.listIssue).toBeDefined();
+    expect(service.updateFinalized).toBeDefined();
     expect(service.filter).toBeDefined();
   });
 
   it('createMessageMethod_should_evoke_createMessage_by_repositoryService', () => {
     service.createMessage(Mock._id, Mock);
-    expect(Injections.ProjectCommunicationRepositoryService.createMessage).toHaveBeenCalledTimes(1)
+    expect(Injections.ProjectCommunicationRepositoryService.createMessage).toHaveBeenCalledTimes(1);
   });
 
   it('createIssueMethod_should_evoke_createIssue_by_repositoryService', () => {
     service.createIssue(Mock);
-    expect(Injections.ProjectCommunicationRepositoryService.createIssue).toHaveBeenCalledTimes(1)
+    expect(Injections.ProjectCommunicationRepositoryService.createIssue).toHaveBeenCalledTimes(1);
   });
 
   it('getProjectCommunicationByIdMethod_should_evoke_getProjectCommunicationById_by_repositoryService', () => {
     service.getProjectCommunicationById(Mock._id);
-    expect(Injections.ProjectCommunicationRepositoryService.getProjectCommunicationById).toHaveBeenCalledTimes(1)
+    expect(Injections.ProjectCommunicationRepositoryService.getProjectCommunicationById).toHaveBeenCalledTimes(1);
   });
 
   it('getProjectCommunicationByIdLimitMethod_should_evoke_getProjectCommunicationByIdLimit_by_repositoryService', () => {
     service.getProjectCommunicationByIdLimit(Mock._id, LIMIT);
-    expect(Injections.ProjectCommunicationRepositoryService.getProjectCommunicationByIdLimit).toHaveBeenCalledTimes(1)
+    expect(Injections.ProjectCommunicationRepositoryService.getProjectCommunicationByIdLimit).toHaveBeenCalledTimes(1);
   });
 
   it('updateReopenMethod_should_evoke_updateReope_by_repositoryService', () => {
     service.updateReopen(Mock._id);
-    expect(Injections.ProjectCommunicationRepositoryService.updateReopen).toHaveBeenCalledTimes(1)
+    expect(Injections.ProjectCommunicationRepositoryService.updateReopen).toHaveBeenCalledTimes(1);
   });
 
   it('updateCloseMethod_should_evoke_updateClose_by_repositoryService', () => {
     service.updateClose(Mock._id);
-    expect(Injections.ProjectCommunicationRepositoryService.updateClose).toHaveBeenCalledTimes(1)
+    expect(Injections.ProjectCommunicationRepositoryService.updateClose).toHaveBeenCalledTimes(1);
   });
 
-  it('listIssueMethod_should_evoke_listIssue_by_repositoryService', () => {
-    service.listIssue();
-    expect(Injections.ProjectCommunicationRepositoryService.listIssue).toHaveBeenCalledTimes(1)
+  it('updateFinalizedMethod_should_evoke_listIssue_by_repositoryService', () => {
+    service.updateFinalized();
+    expect(Injections.ProjectCommunicationRepositoryService.updateFinalized).toHaveBeenCalledTimes(1);
   });
 
   it('filterMethod_should_evoke_filter_by_repositoryService', () => {
     service.filter(Mock);
-    expect(Injections.ProjectCommunicationRepositoryService.filter).toHaveBeenCalledTimes(1)
+    expect(Injections.ProjectCommunicationRepositoryService.filter).toHaveBeenCalledTimes(1);
+  });
+
+  it('getAllIssueMessagesMethod_should_evoke_filter_by_repositoryService', () => {
+    service.getAllIssueMessages(Mock);
+    expect(Injections.ProjectCommunicationRepositoryService.getAllIssueMessages).toHaveBeenCalledTimes(1);
+  });
+
+  it('getIssueSenderInfoMethod_should_evoke_filter_by_repositoryService', () => {
+    service.getIssueSenderInfo(Mock);
+    expect(Injections.ProjectCommunicationRepositoryService.getIssueSenderInfo).toHaveBeenCalledTimes(1);
   });
 
 });
