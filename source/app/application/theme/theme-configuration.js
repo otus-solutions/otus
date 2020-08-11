1c713d96f2164bf20299ef316dd3b5b647b293ef
FlaviaAP <flavia.avila@ufrgs.br>
2020-08-10 22:23:50 -0300

    OTUS-767 theme-config com customização da paleta de cores do tema

--- source/app/application/theme/theme-configuration.js
+++ source/app/application/theme/theme-configuration.js
@@ -13,24 +13,16 @@
 
   function Configuration($mdThemingProvider, $mdIconProvider, THEME_CONSTANTS) {
 
-    const newLightBlueMap = $mdThemingProvider.extendPalette('light-blue', {
-      '500': '#ffffff',
-      'contrastDarkColors': ['500','A100','A400']
-    });
-    $mdThemingProvider.definePalette('new-light-blue', newLightBlueMap);
-
     /* Configuration icons - 24 is the size default of icons */
     $mdIconProvider.defaultIconSet('static-resource/image/icons/mdi.svg', 24);
 
-    _setTheme(THEME_CONSTANTS.theme);
+    _setTheme(THEME_CONSTANTS);
 
     fetch("app/static-resource/visual-identity/data.json")
       .then(response => {
         if(response.ok){
           response.json()
-            .then(data => {
-              _setTheme(data.theme)
-            })
+            .then(data => _setTheme(data))
             .catch(error => console.log(error));
         }
       })
@@ -38,10 +30,14 @@
         console.log(error);
       });
 
+    function _setTheme(paletteThemeObj) {
+      console.log(JSON.stringify(paletteThemeObj, null, 2))//.
 
-    function _setTheme(theme){
-      console.log(JSON.stringify(theme, null, 2))//.
+      const palette = paletteThemeObj.palette;
+      const newMap = $mdThemingProvider.extendPalette(palette.baseName, palette.map);
+      $mdThemingProvider.definePalette(palette.newName, newMap);
 
+      const theme = paletteThemeObj.theme;
       $mdThemingProvider.theme('default')
         .primaryPalette(theme.primary.main, theme.primary.pallete)
         .accentPalette(theme.accent.main, theme.accent.pallete)
