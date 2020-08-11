7db63170d4c083c4797041e9e70b47a1173faba3
FlaviaAP <flavia.avila@ufrgs.br>
2020-08-10 22:11:55 -0300

    OTUS-767 theme-config com carregamento de json com theme customizavel funcionando

--- source/app/application/theme/theme-configuration.js
+++ source/app/application/theme/theme-configuration.js
@@ -19,35 +19,30 @@
     });
     $mdThemingProvider.definePalette('new-light-blue', newLightBlueMap);
 
-    /*Configuration icons*/
-    /* 24 is the size default of icons */
+    /* Configuration icons - 24 is the size default of icons */
     $mdIconProvider.defaultIconSet('static-resource/image/icons/mdi.svg', 24);
 
-    _setTheme('default', THEME_CONSTANTS.DEFAULT_THEME);
+    _setTheme(THEME_CONSTANTS.theme);
 
     fetch("app/static-resource/visual-identity/data.json")
       .then(response => {
-        //console.log(response)
         if(response.ok){
-          console.log('TEM');
           response.json()
             .then(data => {
-              console.log(JSON.stringify(data, null, 2))//.
-              _setTheme('default', data)
+              _setTheme(data.theme)
             })
-            .catch(error => console.log('ops 1', error));
-        }
-        else{
-          console.log('NAO TEM');
+            .catch(error => console.log(error));
         }
       })
       .catch(error => {
-        console.log('ops 2', error);
+        console.log(error);
       });
 
 
-    function _setTheme(themeName, theme){
-      $mdThemingProvider.theme(themeName)
+    function _setTheme(theme){
+      console.log(JSON.stringify(theme, null, 2))//.
+
+      $mdThemingProvider.theme('default')
         .primaryPalette(theme.primary.main, theme.primary.pallete)
         .accentPalette(theme.accent.main, theme.accent.pallete)
         .warnPalette(theme.warn.main, theme.warn.pallete)
