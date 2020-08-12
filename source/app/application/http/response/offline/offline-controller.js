f51bfc360350db9fcc6072fbff3e6aef23c5d34c
FlaviaAP <flavia.avila@ufrgs.br>
2020-08-12 15:46:41 -0300

    OTUS-767 template off-line alinhado com theme

--- source/app/application/http/response/offline/offline-controller.js
+++ source/app/application/http/response/offline/offline-controller.js
@@ -6,12 +6,24 @@
     .controller('otusjs.application.http.ResponseErrorOfflineController', Controller);
 
   Controller.$inject = [
-    'otusjs.application.state.ApplicationStateService'
+    'otusjs.application.state.ApplicationStateService',
+    '$mdColors',
+    'THEME_CONSTANTS'
   ];
 
-  function Controller(ApplicationStateService) {
-    var self = this;
+  function Controller(ApplicationStateService, $mdColors, THEME_CONSTANTS) {
+    const self = this;
+    self.$onInit = onInit;
     self.tryAgain = tryAgain;
+
+    function onInit(){
+      self.title = THEME_CONSTANTS.projectName;
+      self.crashImageURL = THEME_CONSTANTS.iconLogoURL;
+      self.titleStyle = {
+        backgroundColor: $mdColors.getThemeColor('default-primary'),
+        color: $mdColors.getThemeColor('accent-hue-1')
+      };
+    }
 
     function tryAgain() {
       ApplicationStateService.activateLogin();
