62152088e3b6497650871b04b08cfd25a3d02ae1
FlaviaAP <flavia.avila@ufrgs.br>
2020-11-18 16:47:02 -0300

    OFE-112 finalização de laboratory-rest-service, com chamadas dos rest
    excluídos retornos "dummy"

--- source/app/deploy/rest/otus-api/laboratory/laboratory-rest-service.js
+++ source/app/deploy/rest/otus-api/laboratory/laboratory-rest-service.js
@@ -124,12 +124,11 @@
     }
 
     function updateTubeCustomMetadata(tube){
-      if (!_configurationRest) {
+      if (!_participantRest) {
         throw new Error('REST resource is no initialized.');
       }
-      return Promise.resolve(true);//TODO
+      return _participantRest.updateTubeCustomMetadata(tube).$promise;
     }
-
 
     /* laboratory-configuration methods */
     function getDescriptors() {
@@ -164,7 +163,7 @@
       if (!_configurationRest) {
         throw new Error('REST resource is no initialized.');
       }
-      return Promise.resolve(_getDummyData(tubeType));//TODO
+      return _configurationRest.getTubeMedataDataByType({type: tubeType}).$promise;
     }
 
 
@@ -210,119 +209,6 @@
         throw new Error('REST resource is no initialized.');
       }
       return _unattachedRest.getByIdentification({laboratoryIdentification:laboratoryIdentification}).$promise;
-    }
-
-
-    //TODO remover
-    function _getDummyData(tubeType) { //TODO temporario
-      const dummyData = [
-        {
-          "_id": "5fb410071fa5e63b532dedf7",
-          "objectType": "TubeCustomMetadata",
-          "type": "GEL",
-          "value": "Adequada",
-          "extractionValue": "Adequada"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dedf8",
-          "objectType": "TubeCustomMetadata",
-          "type": "GEL",
-          "value": "Inadequada - Aberto",
-          "extractionValue": "Inadequada - Aberto"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dedf9",
-          "objectType": "TubeCustomMetadata",
-          "type": "GEL",
-          "value": "Inadequada - Cassete quebrado",
-          "extractionValue": "Inadequada - Cassete quebrado"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dedfa",
-          "objectType": "TubeCustomMetadata",
-          "type": "GEL",
-          "value": "Inadequado - Outro",
-          "extractionValue": "Inadequado - Outro"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dedfb",
-          "objectType": "TubeCustomMetadata",
-          "type": "EDTA",
-          "value": "Adequada",
-          "extractionValue": "Adequada"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dedfc",
-          "objectType": "TubeCustomMetadata",
-          "type": "EDTA",
-          "value": "Inadequada - Vazado",
-          "extractionValue": "Inadequada - Vazado"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dedfd",
-          "objectType": "TubeCustomMetadata",
-          "type": "EDTA",
-          "value": "Inadequada - Vazio",
-          "extractionValue": "Inadequada - Vazio"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dedfe",
-          "objectType": "TubeCustomMetadata",
-          "type": "EDTA",
-          "value": "Inadequada - Volume inferior a 600uL",
-          "extractionValue": "Inadequada - Volume inferior a 600uL"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dedff",
-          "objectType": "TubeCustomMetadata",
-          "type": "EDTA",
-          "value": "Inadequada - Volume superior a 600uL",
-          "extractionValue": "Inadequada - Volume superior a 600uL"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dee00",
-          "objectType": "TubeCustomMetadata",
-          "type": "EDTA",
-          "value": "Inadequado - Outro",
-          "extractionValue": "Inadequado - Outro"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dee01",
-          "objectType": "TubeCustomMetadata",
-          "type": "CRYOTUBE",
-          "value": "Adequada",
-          "extractionValue": "Adequada"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dee02",
-          "objectType": "TubeCustomMetadata",
-          "type": "CRYOTUBE",
-          "value": "Inadequada - Vazado",
-          "extractionValue": "Inadequada - Vazado"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dee03",
-          "objectType": "TubeCustomMetadata",
-          "type": "CRYOTUBE",
-          "value": "Inadequada - Vazio",
-          "extractionValue": "Inadequada - Vazio"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dee04",
-          "objectType": "TubeCustomMetadata",
-          "type": "CRYOTUBE",
-          "value": "Inadequada - quebrado",
-          "extractionValue": "Inadequada - quebrado"
-        },
-        {
-          "_id": "5fb410071fa5e63b532dee05",
-          "objectType": "TubeCustomMetadata",
-          "type": "CRYOTUBE",
-          "value": "Inadequado - Outro",
-          "extractionValue": "Inadequado - Outro"
-        }
-      ];
-      return dummyData.filter(obj => obj.type === tubeType);
     }
 
   }
