223d9642e86654d31278bb733c43602396ca21e0
FlaviaAP <flavia.avila@ufrgs.br>
2020-11-19 20:22:46 -0300

    OFE-112 ajuste no método chamado para update de customMetadata no controller

--- source/app/ux-component/laboratory/laboratory-material-manager/laboratory-material-manager-dashboard-controller.js
+++ source/app/ux-component/laboratory/laboratory-material-manager/laboratory-material-manager-dashboard-controller.js
@@ -32,7 +32,7 @@
     self.saveChangedTubes = saveChangedTubes;
     self.cancelTube = cancelTube;
     self.saveMetadata = saveMetadata;
-    self.updateCustomMetadata = updateCustomMetadata;
+    self.updateTubeCustomMetadata = updateTubeCustomMetadata;
 
     function onInit() {
       LoadingScreenService.start();
@@ -58,11 +58,17 @@
               .then(data => {
                 self.tubeCustomMetadataOptions = data.map(obj => angular.extend(obj, obj, {selected: false}));
 
-                if(self.originalTube.customMetadata){
+                if(self.originalTube.tubeCollectionData.customMetadata){
                   self.tubeCustomMetadataOptions
-                    .filter(obj => self.originalTube.customMetadata.includes(obj._id))
+                    .filter(obj => self.originalTube.tubeCollectionData.customMetadata.includes(obj._id))
                     .forEach(obj => obj.selected = true);
                 }
+                else{
+                  self.originalTube.tubeCollectionData.customMetadata = [];
+                }
+
+                console.log(self.tubeCustomMetadataOptions)
+                console.log('self.originalTube.tubeCollectionData.customMetadata\n', JSON.stringify(self.originalTube.tubeCollectionData.customMetadata));
               })
               .catch(e => _showToastMsg('Tipo de tubo não encontrado'));
           })
@@ -104,17 +110,31 @@
       }
     }
 
-    function updateCustomMetadata(){
+    function updateTubeCustomMetadata(option){
       const newTube = angular.copy(self.originalTube);
-      newTube.customMetadata = self.tubeCustomMetadataOptions
-        .filter(option => option.selected)
-        .map(obj => obj._id);
+
+      const value = {
+        _id: option._id,
+        value: option.value
+      };///.
+
+      if(option.selected){
+        console.log('adicionar\n', JSON.stringify(value, null, 2));
+        newTube.tubeCollectionData.customMetadata.push(option._id);
+      }
+      else{
+        console.log('remover\n', JSON.stringify(value, null, 2));
+        newTube.tubeCollectionData.customMetadata = newTube.tubeCollectionData.customMetadata.filter(id => id !== option._id);
+      }
+
+      console.log('newTube.tubeCollectionData.customMetadata\n', JSON.stringify(newTube.tubeCollectionData.customMetadata));//.
 
       ParticipantLaboratoryService.updateTubeCustomMetadata(newTube)
         .then(response => {
-          self.originalTube.customMetadata = newTube.customMetadata;
+          self.originalTube.tubeCollectionData.customMetadata = angular.copy(newTube.tubeCollectionData.customMetadata);
+          console.log('new self.originalTube.tubeCollectionData.customMetadata\n', JSON.stringify(self.originalTube.tubeCollectionData.customMetadata));//.
         })
-        .catch(err => _showToastMsg('Falha ao registrar coleta'));
+        .catch(err => _showToastMsg('Falha ao registrar metadado'));
     }
 
     function _updateChangedTubes() {
