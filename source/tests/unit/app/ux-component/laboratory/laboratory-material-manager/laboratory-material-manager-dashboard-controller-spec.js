f34fa2c670ee6f8abf282c041a0295220eab3409
FlaviaAP <flavia.avila@ufrgs.br>
2020-11-22 22:58:39 -0300

    OFE-112 ajustes na suite de teste de laboratoryMaterialManagerDashboardCtrl
    aidna incompleto

--- source/tests/unit/app/ux-component/laboratory/laboratory-material-manager/laboratory-material-manager-dashboard-controller-spec.js
+++ source/tests/unit/app/ux-component/laboratory/laboratory-material-manager/laboratory-material-manager-dashboard-controller-spec.js
@@ -1,11 +1,12 @@
 describe('laboratoryMaterialManagerDashboardCtrl_Test_Suite', () => {
-  let ctrl;
+  let controller;
   let Injections = [];
   let Mock = {};
 
   beforeEach(() => {
     angular.mock.module('otusjs.otus');
-    angular.mock.inject(($injector,$controller) => {
+
+    angular.mock.inject(($injector, $controller, $rootScope) => {
       Injections.$mdToast = $injector.get('$mdToast');
       Injections.$mdDialog = $injector.get('$mdDialog');
       Injections.$filter = $injector.get('$filter');
@@ -14,47 +15,124 @@ describe('laboratoryMaterialManagerDashboardCtrl_Test_Suite', () => {
       Injections.ParticipantManagerService = $injector.get('otusjs.participant.business.ParticipantManagerService');
       Injections.DialogShowService = $injector.get('otusjs.application.dialog.DialogShowService');
 
-      ctrl = $controller('laboratoryMaterialManagerDashboardCtrl', Injections);
+      controller = $controller('laboratoryMaterialManagerDashboardCtrl', Injections);
+
+      Mock.$scope = $rootScope.$new();
     });
 
     _mockInitialize();
+    spyOn(Injections.$mdToast, 'show').and.callThrough();
   });
 
-  it('ctrl_existence_check', () => {
-    expect(ctrl).toBeDefined();
+  it('controller_existence_check', () => {
+    expect(controller).toBeDefined();
   });
 
-  it('ctrl_methods_existence_check', () => {
-    expect(ctrl.$onInit).toBeDefined();
-    expect(ctrl.isValidCode).toBeDefined();
-    expect(ctrl.tubeHasCustomMetadata).toBeDefined();
-    expect(ctrl.originalTubeHasCode).toBeDefined();
-    expect(ctrl.saveChangedTubes).toBeDefined();
-    expect(ctrl.cancelTube).toBeDefined();
-    expect(ctrl.saveMetadata).toBeDefined();
-    expect(ctrl.updateTubeCustomMetadata).toBeDefined();
-    expect(ctrl.isEnterKey).toBeDefined();
+  it('controller_methods_existence_check', () => {
+    expect(controller.$onInit).toBeDefined();
+    expect(controller.isValidCode).toBeDefined();
+    expect(controller.tubeHasCustomMetadata).toBeDefined();
+    expect(controller.originalTubeHasCode).toBeDefined();
+    expect(controller.saveChangedTubes).toBeDefined();
+    expect(controller.cancelTube).toBeDefined();
+    expect(controller.saveMetadata).toBeDefined();
+    expect(controller.updateTubeCustomMetadata).toBeDefined();
+    expect(controller.isEnterKey).toBeDefined();
   });
 
   describe('isValidCode_method', () => {
 
-    beforeEach(() => {
-      spyOn(Injections.ParticipantLaboratoryService, 'getTubeMedataDataByType').and.returnValue(_mockPromiseResolve(Mock.tube));
+    xit('should_show_toast_error_in_case_ParticipantLaboratoryService_getLaboratoryByTube_return_rejected_promise', () => {
+      spyOn(Injections.ParticipantLaboratoryService, 'getLaboratoryByTube').and.returnValue(Mock.reject);
+      controller.isValidCode(Mock.tube.code);
+      Mock.$scope.$digest();
+      _expectShowToastError();
+    });
+
+    describe('ParticipantLaboratoryService_getLaboratoryByTube_return_resolved_promise', () => {
+      beforeEach(() => {
+        spyOn(Injections.ParticipantLaboratoryService, 'getLaboratoryByTube').and.returnValue(_mockPromiseResolve(Mock.tube));
+      });
+    });
+
+  });
+
+  describe('tubeHasCustomMetadata_method', () => {
+
+    it('should_return_false_in_case_tubeCustomMetadataOptions_not_defined', () => {
+      controller.tubeCustomMetadataOptions = null;
+      expect(controller.tubeHasCustomMetadata()).toBeFalsy();
+    });
+
+    it('should_return_false_in_case_tubeCustomMetadataOptions_is_empty_array', () => {
+      controller.tubeCustomMetadataOptions = [];
+      expect(controller.tubeHasCustomMetadata()).toBe(false);
+    });
+
+    it('should_return_true_in_case_tubeCustomMetadataOptions_is_not_empty_array', () => {
+      controller.tubeCustomMetadataOptions = [{}];
+      expect(controller.tubeHasCustomMetadata()).toBe(true);
+    });
+  });
+
+  it('originalTubeHasCode_method_should_return_true', () => {
+    expect(controller.originalTubeHasCode()).toBe(true);
+  });
+
+  describe('updateTubeCustomMetadata_method', () => {
+
+    it('should_call_originalTube_pushCustomMetadata_method_in_case_customMetadata_is_selected', () => {
+      spyOn(controller.originalTube, 'pushCustomMetadata');
+      controller.updateTubeCustomMetadata(Mock.customMetadataOption);
+      expect(controller.originalTube.pushCustomMetadata).toHaveBeenCalledWith(Mock.customMetadataOption._id);
+    });
+
+    it('should_call_originalTube_removeCustomMetadata_method_in_case_customMetadata_is_selected', () => {
+      spyOn(controller.originalTube, 'removeCustomMetadata');
+      Mock.customMetadataOption.selected = false;
+      controller.updateTubeCustomMetadata(Mock.customMetadataOption);
+      expect(controller.originalTube.removeCustomMetadata).toHaveBeenCalledWith(Mock.customMetadataOption._id);
+    });
+
+    it('should_do_nothing_in_case_ParticipantLaboratoryService_updateTubeCustomMetadata_return_resolved_promise', () => {
+      spyOn(Injections.ParticipantLaboratoryService, 'updateTubeCustomMetadata').and.returnValue(Mock.resolve);
+      controller.updateTubeCustomMetadata(Mock.customMetadataOption);
+      Mock.$scope.$digest();
+    });
+
+    xit('should_show_toast_error_in_case_ParticipantLaboratoryService_updateTubeCustomMetadata_return_rejected_promise', () => {
+      spyOn(Injections.ParticipantLaboratoryService, 'updateTubeCustomMetadata').and.returnValue(Mock.reject);
+      controller.updateTubeCustomMetadata(Mock.customMetadataOption);
+      Mock.$scope.$digest();
+      _expectShowToastError();
     });
 
   });
 
 
+
   function _mockInitialize() {
-    Mock.tube = Test.utils.data.tube;
+    Mock.resolve = Promise.resolve({});
+    Mock.reject = Promise.reject('some error');
+
+    Mock.tube = angular.copy(Test.utils.data.tube);
+    Mock.tube.hasOwnProperty = function(propertyName){ return true };
+    Mock.tube.pushCustomMetadata = function(x) { };
+    Mock.tube.removeCustomMetadata = function(x) { };
+    controller.originalTube = Mock.tube;
+
+    Mock.customMetadataOption = {
+      _id: 'abc',
+      selected: true
+    }
   }
 
   function _mockPromiseResolve(value){
     return Promise.resolve(value);
+  }
 
-    // const deferredResolveObj = Injections.$q.defer();
-    // deferredResolveObj.resolve(value);
-    // return deferredResolveObj.promise;
+  function _expectShowToastError(){
+    expect(Injections.$mdToast.show).toHaveBeenCalledTimes(1);
   }
 
 });
