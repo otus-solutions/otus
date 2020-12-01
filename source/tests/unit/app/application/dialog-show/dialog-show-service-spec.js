c76dc2b652a50c5c9e8596897add87568ecd788a
FlaviaAP <flavia.avila@ufrgs.br>
2020-12-01 19:31:52 -0300

    OTUS-811 atualização de dialog-show-service-spec

--- source/tests/unit/app/application/dialog-show/dialog-show-service-spec.js
+++ source/tests/unit/app/application/dialog-show/dialog-show-service-spec.js
@@ -1,56 +1,51 @@
-describe('dialogShowService', function () {
+describe('DialogShowService_UnitTest_Suite', function () {
   var service;
   var Mock = {};
-  var Injections;
+  var Injections = [];
 
 
   beforeEach(function () {
-    angular.mock.module('otusjs.application.dialog');
-    Mock.mdDialog = {
-      show: function (dialog) {
-        var self = this;
-        self.test = dialog;
-        return Promise.resolve(self);
-      }
-    };
+    angular.mock.module('otusjs.otus');
 
-    angular.mock.module(function ($provide) {
-      $provide.value('$mdDialog', Mock.mdDialog);
-    });
-
-    angular.mock.inject(function (_$injector_) {
-      Injections = {
-        $mdDialog: _$injector_.get('$mdDialog')
-      };
-      service = _$injector_.get('otusjs.application.dialog.DialogShowService', Injections);
+    angular.mock.inject(function ($injector) {
+      Injections.$mdDialog = $injector.get('$mdDialog');
+      service = $injector.get('otusjs.application.dialog.DialogShowService', Injections);
     });
 
     mockData();
-    spyOn(Mock.mdDialog, 'show').and.callThrough();
-
+    spyOn(Injections.$mdDialog, 'show').and.callThrough();
   });
 
-  it('serviceExistence check', function () {
+  it('service_existence_check', function () {
     expect(service).toBeDefined();
+  });
+
+  it('service_method_existence_check', function () {
+    expect(service.showDialog).toBeDefined();
+    expect(service.showConfirmationDialog).toBeDefined();
+    expect(service.showCustomizedDialog).toBeDefined();
+    expect(service.showActivitySharingDialog).toBeDefined();
   });
 
   describe('serviceInstance', function () {
 
-    it('showDialogMethodExistence check', function () {
-      expect(service.showDialog).toBeDefined();
-      expect(service.showConfirmationDialog).toBeDefined();
-      expect(service.showCustomizedDialog).toBeDefined();
-      expect(service.showActivitySharingDialog).toBeDefined();
-    });
-
-    it('should showDialog an instance with the same values', function () {
+    it('should_showDialog_an_instance_with_the_same_values', function () {
       service.showDialog(Mock.data).then(function (serviceShowResult) {
         expect(serviceShowResult.test.locals.data).toEqual(Mock.data);
       });
     });
 
-    it('should showConfirmationDialog an instance with the same values', function () {
+    it('should_showConfirmationDialog_an_instance_with_the_same_values', function () {
       service.showConfirmationDialog(Mock.data.titleToText, Mock.data.textDialog, Mock.data.ariaLabel).then(function (serviceShowResult) {
+        expect(serviceShowResult.test.locals.data.dialogToTitle).toEqual(Mock.data.titleToText);
+        expect(serviceShowResult.test.locals.data.titleToText).toEqual(Mock.data.titleToText);
+        expect(serviceShowResult.test.locals.data.textDialog).toEqual(Mock.data.textDialog);
+        expect(serviceShowResult.test.locals.data.ariaLabel).toEqual(Mock.data.ariaLabel);
+      });
+    });
+
+    it('should_showWarningDialog_an_instance_with_the_same_values', function () {
+      service.showWarningDialog(Mock.data.dialogToTitle, Mock.data.titleToText, Mock.data.textDialog, Mock.data.ariaLabel).then(function (serviceShowResult) {
         expect(serviceShowResult.test.locals.data.dialogToTitle).toEqual("Confirmação");
         expect(serviceShowResult.test.locals.data.titleToText).toEqual(Mock.data.titleToText);
         expect(serviceShowResult.test.locals.data.textDialog).toEqual(Mock.data.textDialog);
@@ -58,13 +53,13 @@ describe('dialogShowService', function () {
       });
     });
 
-    it('should showCustomizedDialog an instance with the same values', function () {
+    it('should_showCustomizedDialog_an_instance_with_the_same_values', function () {
       service.showCustomizedDialog(Mock.data).then(function (serviceShowResult) {
         expect(serviceShowResult.test.locals.data).toEqual(Mock.data);
       });
     });
 
-    it('should showActivitySharingDialog an instance with the same values', function () {
+    it('should_showActivitySharingDialog_an_instance_with_the_same_values', function () {
       service.showActivitySharingDialog(Mock.data2.activity).then(function (serviceShowResult) {
         expect(serviceShowResult.test.locals.data.activity).toEqual(Mock.data2.activity);
       });
@@ -83,7 +78,7 @@ describe('dialogShowService', function () {
       buttons: [
         {
           message: 'Ok',
-          action: function () { $mdDialog.hide() },
+          action: function () { Injections.$mdDialog.hide() },
           class: 'md-raised md-primary'
         }
       ]
