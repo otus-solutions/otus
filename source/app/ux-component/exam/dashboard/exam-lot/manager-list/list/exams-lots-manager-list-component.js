caf9900e26efa7d4085e52f859f193ecbd8b6296
FlaviaAP <flavia.avila@ufrgs.br>
2020-12-14 20:51:25 -0300

    OTUS-814 refatoração em exams-lots-manager-list-component

--- source/app/ux-component/exam/dashboard/exam-lot/manager-list/list/exams-lots-manager-list-component.js
+++ source/app/ux-component/exam/dashboard/exam-lot/manager-list/list/exams-lots-manager-list-component.js
@@ -29,8 +29,8 @@
     ProjectFieldCenterService,
     ExamLotService,
     $mdToast,
-    laboratoryContextService,
-    dashboardContextService,
+    LaboratoryContextService,
+    DashboardContextService,
     $filter,
     LaboratoryViewerService) {
     var self = this;
@@ -81,16 +81,15 @@
     }
 
     function _setUserFieldCenter() {
-      dashboardContextService
-        .getLoggedUser()
+      DashboardContextService.getLoggedUser()
         .then(function(userData) {
           self.userHaveCenter = !!userData.fieldCenter.acronym;
-          self.centerFilter = self.userHaveCenter ? userData.fieldCenter.acronym : laboratoryContextService.getSelectedExamLotFieldCenter() ? laboratoryContextService.getSelectedExamLotFieldCenter() : "";
+          self.centerFilter = self.userHaveCenter ? userData.fieldCenter.acronym : LaboratoryContextService.getSelectedExamLotFieldCenter() ? LaboratoryContextService.getSelectedExamLotFieldCenter() : "";
           if(!self.centerFilter){
             self.centerFilter = self.centers[0];
           }
           loadExamDescriptors(self.centerFilter);
-          laboratoryContextService.setSelectedExamLotFieldCenter(self.centerFilter);
+          LaboratoryContextService.setSelectedExamLotFieldCenter(self.centerFilter);
           self.centerFilterDisabled = userData.fieldCenter.acronym ? "disabled" : "";
           _LoadLotsList();
         });
@@ -104,7 +103,6 @@
       } else {
         self.selectedLots.push(lot);
         lot.isSelected = true;
-
       }
     }
 
@@ -140,40 +138,37 @@
 
     function _filterByPeriod(FilteredByCenter) {
       var lotFormattedData = $filter('date')(FilteredByCenter.realizationDate, 'yyyyMMdd');
-      if (self.realizationBeginFilter && self.realizationEndFilter) {
-        var initialDateFormatted = $filter('date')(self.realizationBeginFilter, 'yyyyMMdd');
-        var finalDateFormatted = $filter('date')(self.realizationEndFilter, 'yyyyMMdd');
-        if(initialDateFormatted <= finalDateFormatted){
-          return (lotFormattedData >= initialDateFormatted && lotFormattedData <= finalDateFormatted);
-        }else{
-          var msgDataInvalida = "Datas invalidas";
-
-          $mdToast.show(
-            $mdToast.simple()
-              .textContent(msgDataInvalida)
-              .hideDelay(4000)
-          );
-          return FilteredByCenter;
-        }
-      } else {
+      if (!self.realizationBeginFilter || !self.realizationEndFilter) {
         return FilteredByCenter;
       }
+
+      var initialDateFormatted = $filter('date')(self.realizationBeginFilter, 'yyyyMMdd');
+      var finalDateFormatted = $filter('date')(self.realizationEndFilter, 'yyyyMMdd');
+      if(initialDateFormatted <= finalDateFormatted){
+        return (lotFormattedData >= initialDateFormatted && lotFormattedData <= finalDateFormatted);
+      }
+
+      $mdToast.show(
+        $mdToast.simple()
+          .textContent("Datas invalidas")
+          .hideDelay(4000)
+      );
+      return FilteredByCenter;
     }
 
     function _filterByExam(filteredByPeriod) {
       if (self.examFilter.length && self.examFilter !== "ALL") {
         return filteredByPeriod.aliquotName === self.examFilter;
-      } else {
-        return filteredByPeriod;
       }
+      return filteredByPeriod;
     }
 
     function _setSessionData(){
       if(self.centerFilter.length){
-        laboratoryContextService.setSelectedExamLotFieldCenter(self.centerFilter);
+        LaboratoryContextService.setSelectedExamLotFieldCenter(self.centerFilter);
       }
       if(self.centerFilter.length){
-        laboratoryContextService.setSelectedExamType(self.examFilter);
+        LaboratoryContextService.setSelectedExamType(self.examFilter);
       }
     }
   }
