183f6f84dfa74eca3b21a92270a36f83cfbddd2c
Adonis <adonis.garcia.adg@gmail.com>
2021-04-14 10:25:29 -0300

    OTUS-908 Tela de dashboard da home do participante

--- source/app/ux-component/participant-info/participant-info-component.js
+++ source/app/ux-component/participant-info/participant-info-component.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -16,17 +16,13 @@
     'otusjs.application.state.ApplicationStateService',
     'otusjs.otus.dashboard.core.EventService',
     'otusjs.participant.core.EventService',
-    'otusjs.otus.dashboard.service.DashboardService',
-    'otusjs.participant.business.ParticipantLabelFactory',
-    'otusjs.otus.uxComponent.Publisher'
+    'otusjs.otus.dashboard.service.DashboardService'
   ];
 
   function Controller(ApplicationStateService,
-                      EventService,
-                      ParticipantEventService,
-                      DashboardService,
-                      ParticipantLabelFactory,
-                      Publisher) {
+    EventService,
+    ParticipantEventService,
+    DashboardService) {
     var self = this;
 
     self.participantBirthdate;
@@ -37,15 +33,15 @@
     /* Lifecycle hooks */
     self.$onInit = onInit;
     self.getCurrentState = getCurrentState;
+    self.updateParticipant = updateParticipant;
 
     self.getSex = getSex;
 
-    function getSex(sex){
-      if(sex){
+    function getSex(sex) {
+      if (sex) {
         return sex == 'F' || sex == 'f' ? 'Feminino' : 'Masculino';
       }
     }
-
 
     function selectParticipant(selectedParticipant) {
       self.selectedParticipant = selectedParticipant;
@@ -53,7 +49,7 @@
 
     /* Lifecycle methods */
     function onInit() {
-       _loadSelectedParticipant();
+      _loadSelectedParticipant();
       EventService.onParticipantSelected(_loadSelectedParticipant);
       self.selectedParticipant = null;
     }
@@ -67,43 +63,20 @@
         self.selectedParticipant = participantData;
         self.participantBirthdate = self.selectedParticipant.birthdate ? new Date(self.selectedParticipant.birthdate.value) : '';
         self.isEmpty = false;
-        self.participantLabel = ParticipantLabelFactory.create(self.selectedParticipant)
-        ParticipantEventService.fireParticipantLoaded(participantData)
-        _publishPrintStructure()
-        _subscribeLabels()
+        ParticipantEventService.fireParticipantLoaded(participantData);
       } else {
         DashboardService
-        .getSelectedParticipant()
-        .then(function(participantData) {
+          .getSelectedParticipant()
+          .then(function (participantData) {
             self.selectedParticipant = participantData;
-            ParticipantEventService.fireParticipantLoaded(participantData)
+            ParticipantEventService.fireParticipantLoaded(participantData);
             self.participantBirthdate = self.selectedParticipant.birthdate ? new Date(self.selectedParticipant.birthdate.value) : '';
             self.isEmpty = false;
-            self.participantLabel = ParticipantLabelFactory.create(self.selectedParticipant)
-            _publishPrintStructure()
-            _subscribeLabels()
           });
       }
     }
 
-    function _labelToPrint(callback) {
-      callback(
-        self.participantLabel
-      )
-    }
-
-    function _subscribeLabels() {
-      Publisher.unsubscribe('label-to-print')
-      Publisher.subscribe('label-to-print', _labelToPrint)
-    }
-
-    function _publishPrintStructure() {
-      Publisher.publish("default-print-structure", (defaultPrintStructure) => {
-        self.participantLabel.printStructure = defaultPrintStructure
-      })
-    }
-
-    self.updateParticipant = function () {
+    function updateParticipant() {
       ApplicationStateService.activateUpdateParticipant();
     }
   }
